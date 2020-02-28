82cc704c0c9c0e8c63c4c17817c90cea11631ee1
Brett <b.crunkhorn@uq.edu.au>
2020-02-28 15:35:28 +1000

    added button icon example

--- packages/storybook-html/stories/button.stories.js
+++ packages/storybook-html/stories/button.stories.js
@@ -3,6 +3,7 @@ import '../src/components/button/button.scss';
 
 // import HTML template strings
 import buttonHTML from '../src/components/button/button.html';
+import iconButtonHTML from '../src/components/button/icon-button.html';
 
 export default {
   title: 'Button'
@@ -11,3 +12,8 @@ export default {
 export const button = () => {
   return document.createElement('div').innerHTML = buttonHTML;
 };
+
+export const iconButton = () => {
+  return document.createElement('div').innerHTML = iconButtonHTML;
+};
+

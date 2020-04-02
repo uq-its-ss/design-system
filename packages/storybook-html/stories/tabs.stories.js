4b5a7e6d1744ad18cc4b9913c5473cab2209a5aa
Brett <b.crunkhorn@uq.edu.au>
2020-04-02 17:08:12 +1000

    updated

--- packages/storybook-html/stories/tabs.stories.js
+++ packages/storybook-html/stories/tabs.stories.js
@@ -4,12 +4,10 @@
 import '../src/components/tabs/tabs.scss';
 
 // import scripts
-//import tabsUqCreate from '@uq-uidf/tabs/src/js/tabsuq.js';
-//import tabsQuickCreate from '@uq-uidf/tabs/src/js/tabsquick.js';
+import tabsUqCreate from '@uq-uidf/tabs/src/js/tabs.js';
 
 // import HTML template strings
 import tabsHTML from '../src/components/tabs/tabs.html';
-//import tabsQuickHTML from '../src/components/tabs/tabsquick.html';
 
 export default {
   title: 'Tabs'
@@ -18,8 +16,3 @@ export default {
 export const tabs = () => {
   return document.createElement('div').innerHTML = tabsHTML;
 };
-/*
-export const tabsQuick = () => {
-  return document.createElement('div').innerHTML = tabsQuickHTML;
-};
-*/

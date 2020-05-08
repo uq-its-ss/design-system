89936445c9a787fdede0890d40dc3e10c9314452
Timmy George <timmy.george@uq.edu.au>
2020-05-08 12:34:14 +1000

    Icon Component Branch

---
+++ packages/storybook-html/src/components/icon/icon.stories.js
@@ -0,0 +1,16 @@
+// import decorators
+import namedSection from '../../decorators/section.js';
+
+// import styles
+import './icon.scss';
+
+// import HTML template strings
+import iconHTML from './icon.html';
+
+export default {
+  title: 'Components/Icon'
+};
+
+export const icon = () => {
+  return namedSection('Icon', iconHTML);
+};

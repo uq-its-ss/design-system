30f5b0258cd50d0d676a58b1a7a0c2ffbef25286
Timmy George <timmy.george@uq.edu.au>
2020-05-30 01:32:52 +1000

    Doc updates + rename html files

--- packages/storybook-html/src/components/pagination/pagination.stories.js
+++ packages/storybook-html/src/components/pagination/pagination.stories.js
@@ -3,9 +3,9 @@ import './pagination.scss';
 
 // import HTML template strings
 import paginationHTML from './pagination.html';
-import paginationFirstStateHTML from './pagination-first-state.html';
-import paginationLastStateHTML from './pagination-last-state.html';
-import paginationMiddleStateHTML from './pagination-middle-state.html';
+import paginationFirstHTML from './pagination-first.html';
+import paginationMiddleHTML from './pagination-middle.html';
+import paginationLastHTML from './pagination-last.html';
 
 export default {
   title: 'Components/Pagination'
@@ -15,14 +15,68 @@ export const pagination = () => {
   return document.createElement('div').innerHTML = paginationHTML;
 };
 
-export const paginationFirstState = () => {
-  return paginationFirstStateHTML;
+export const paginationFirst = () => {
+  return `
+    <ul class="pagination">
+    <li class="pagination__item pagination__item--first pagination__item--current"><a title="Go to page 1" href="#">1</a></li>
+    <li class="pagination__item"><a title="Go to page 2" href="#">2</a></li>
+    <li class="pagination__item"><a title="Go to page 3" href="#">3</a></li>
+    <li class="pagination__item"><a title="Go to page 4" href="#">4</a></li>
+    <li class="pagination__item"><a title="Go to page 5" href="#">5</a></li>
+    <li class="pagination__item"><a title="Go to page 6" href="#">6</a></li>
+    <li class="pagination__item"><a title="Go to page 7" href="#">7</a></li>
+    <li class="pagination__ellipsis pagination__ellipsis--right">…</li>
+    <li class="pagination__item pagination__item--go-next"><a title="Go to next page" href="#">next &rsaquo;</a></li>
+    <li class="pagination__item pagination__item--go-last"><a title="Go to last page" href="#">last &raquo;</a></li>
+  </ul>
+  `;
 };
 
-export const paginationLastState = () => {
-  return paginationLastStateHTML;
+paginationFirst.story = {
+  name: 'Pagination First'
+}
+
+export const paginationMiddle = () => {
+  return `
+    <ul class="pagination">
+      <li class="pagination__item pagination__item--go-first"><a title="Go to first page" href="#">&laquo; first</a></li>
+      <li class="pagination__item pagination__item--go-previous"><a title="Go to prev page" href="#">&lsaquo; prev</a></li>
+      <li class="pagination__ellipsis pagination__ellipsis--left">…</li>
+      <li class="pagination__item"><a title="Go to page 8" href="#">8</a></li>
+      <li class="pagination__item"><a title="Go to page 9" href="#">9</a></li>
+      <li class="pagination__item"><a title="Go to page 10" href="#">10</a></li>
+      <li class="pagination__item pagination__item--current"><a title="Go to page 11" href="#">11</a></li>
+      <li class="pagination__item"><a title="Go to page 12" href="#">12</a></li>
+      <li class="pagination__item"><a title="Go to page 13" href="#">13</a></li>
+      <li class="pagination__item"><a title="Go to page 14" href="#">14</a></li>
+      <li class="pagination__ellipsis pagination__ellipsis--right">…</li>
+      <li class="pagination__item pagination__item--go-next"><a title="Go to next page" href="#">next &rsaquo;</a></li>
+      <li class="pagination__item pagination__item--go-last"><a title="Go to last page" href="#">last &raquo;</a></li>
+    </ul>
+  `;
 };
 
-export const paginationMiddleState = () => {
-  return paginationMiddleStateHTML;
+paginationMiddle.story = {
+  name: 'Pagination Middle'
+}
+
+export const paginationLast = () => {
+  return `
+    <ul class="pagination">
+      <li class="pagination__item pagination__item--go-first"><a title="Go to first page" href="#">&laquo; first</a></li>
+      <li class="pagination__item pagination__item--go-previous"><a title="Go to prev page" href="#">&lsaquo; prev</a></li>
+      <li class="pagination__ellipsis pagination__ellipsis--left">…</li>
+      <li class="pagination__item"><a title="Go to page 15" href="#">15</a></li>
+      <li class="pagination__item"><a title="Go to page 16" href="#">16</a></li>
+      <li class="pagination__item"><a title="Go to page 17" href="#">17</a></li>
+      <li class="pagination__item"><a title="Go to page 18" href="#">18</a></li>
+      <li class="pagination__item"><a title="Go to page 19" href="#">19</a></li>
+      <li class="pagination__item"><a title="Go to page 20" href="#">20</a></li>
+      <li class="pagination__item pagination__item--last pagination__item--current"><a title="Go to page 21" href="#">21</a></li>
+    </ul>
+  `;
 };
+
+paginationLast.story = {
+  name: 'Pagination Last'
+}

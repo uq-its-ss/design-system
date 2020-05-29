1d248e6ee1ad665626b3f29def67b5bfd602718b
Timmy George <timmy.george@uq.edu.au>
2020-05-29 12:00:35 +1000

    Doc files for pagination + unwanted images deleted

--- packages/storybook-html/src/components/pagination/pagination.stories.js
+++ packages/storybook-html/src/components/pagination/pagination.stories.js
@@ -3,6 +3,9 @@ import './pagination.scss';
 
 // import HTML template strings
 import paginationHTML from './pagination.html';
+import paginationFirstStateHTML from './pagination-first-state.html';
+import paginationLastStateHTML from './pagination-last-state.html';
+import paginationMiddleStateHTML from './pagination-middle-state.html';
 
 export default {
   title: 'Components/Pagination'
@@ -10,4 +13,16 @@ export default {
 
 export const pagination = () => {
   return document.createElement('div').innerHTML = paginationHTML;
+};
+
+export const paginationFirstState = () => {
+  return paginationFirstStateHTML;
+};
+
+export const paginationLastState = () => {
+  return paginationLastStateHTML;
+};
+
+export const paginationMiddleState = () => {
+  return paginationMiddleStateHTML;
 };

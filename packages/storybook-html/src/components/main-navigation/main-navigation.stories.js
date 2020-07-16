9e4c7e11cb914db98abc97373501c3a2652b5537
Brett <b.crunkhorn@uq.edu.au>
2020-07-16 12:43:48 +1000

    Setup Main Navigation Sortybook

---
+++ packages/storybook-html/src/components/main-navigation/main-navigation.stories.js
@@ -0,0 +1,11 @@
+import expandedStory from '../../../.storybook/decorators/expanded.js';
+
+// import styles
+import './main-navigation.scss';
+
+// import HTML template strings
+import mainNavigationHTML from './main-navigation.html';
+
+export const mainNavigation = () => {
+  return mainNavigationHTML;
+};

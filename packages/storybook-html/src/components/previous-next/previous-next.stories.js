cf4797e863e552133be6de838d3be2322f801969
Timmy George <timmy.george@uq.edu.au>
2020-07-28 13:00:36 +1000

    Initial build Previous Next component

---
+++ packages/storybook-html/src/components/previous-next/previous-next.stories.js
@@ -0,0 +1,13 @@
+// import styles
+import './previous-next.scss';
+
+// import HTML template strings
+import previousNextHTML from './previous-next.html';
+
+export default {
+  title: 'Components/Previous Next'
+};
+
+export const all = () => {
+  return previousNextHTML;
+};

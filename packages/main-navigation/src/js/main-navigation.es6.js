0c8c6f17fa20404683f96f54ab9a7b0110f3ba7e
Timmy George <timmy.george@uq.edu.au>
2020-07-29 17:28:10 +1000

    Solve nav icon issue while tabing

--- packages/main-navigation/src/js/main-navigation.es6.js
+++ packages/main-navigation/src/js/main-navigation.es6.js
@@ -156,7 +156,7 @@ class MainNavigation {
     } else if (parent === nav.lastElementChild) {
       // If we tab past the last child, toggle this level.
       if (event.key === 'Tab' && event.shiftKey === false) {
-        this.closeLevel(nav, nav.parentNode);
+        //this.closeLevel(nav, nav.parentNode);
       }
     }
 

2e983a069344803aa093cb53fb0ed974d98c297a
Timmy George <timmy.george@uq.edu.au>
2020-07-29 11:50:55 +1000

    Added story + prev-next instances

--- packages/storybook-html/src/components/previous-next/previous-next.stories.js
+++ packages/storybook-html/src/components/previous-next/previous-next.stories.js
@@ -8,6 +8,46 @@ export default {
   title: 'Components/Previous Next'
 };
 
-export const all = () => {
+export const previousNext = () => {
   return previousNextHTML;
 };
+
+previousNext.story = {
+  name: 'Previous Next'
+}
+
+export const previousNextFirst = () => {
+  return `
+    <div class="previous-next">
+      <div class="previous-next__item previous-next__item--hidden">
+        <a href="#" class="previous-next__link-previous">Enrolment basics <span class="previous-next__description">Previous</span></a>
+      </div>
+
+      <div class="previous-next__item">
+        <a href="#" class="previous-next__link-next">How to enrol <span class="previous-next__description">Next</span></a>
+      </div>
+    </div>
+  `;
+};
+
+previousNextFirst.story = {
+  name: 'Previous Next First'
+}
+
+export const previousNextLast = () => {
+  return `
+    <div class="previous-next">
+      <div class="previous-next__item">
+        <a href="#" class="previous-next__link-previous">Enrolment basics <span class="previous-next__description">Previous</span></a>
+      </div>
+
+      <div class="previous-next__item previous-next__item--hidden">
+        <a href="#" class="previous-next__link-next">How to enrol <span class="previous-next__description">Next</span></a>
+      </div>
+    </div>
+  `;
+};
+
+previousNextLast.story = {
+  name: 'Previous Next Last'
+}

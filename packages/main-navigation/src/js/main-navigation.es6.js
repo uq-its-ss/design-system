730747b1715bf0b94a63acbf98ff9f63e15b8187
uqmchu <m.chu@uq.edu.au>
2020-08-04 21:11:51 +1000

    main-navigation: add uq- prefix
    update

--- packages/main-navigation/src/js/main-navigation.es6.js
+++ packages/main-navigation/src/js/main-navigation.es6.js
@@ -76,7 +76,7 @@ class MainNavigation {
     const target = this.nav.querySelectorAll(`.${this.level1Class}`);
     const ariaExpanded = toggle.getAttribute('aria-expanded') === 'true';
 
-    toggle.classList.toggle('main-navigation-header__nav-toggle--close');
+    toggle.classList.toggle('uq-main-navigation-header__nav-toggle--close');
     toggle.setAttribute('aria-expanded', !ariaExpanded);
 
     target.forEach((el) => {

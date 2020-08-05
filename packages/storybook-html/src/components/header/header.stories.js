2f75ef28975fb63942f5d760b560b4f8f37e0e62
uqmchu <m.chu@uq.edu.au>
2020-08-05 17:01:59 +1000

    header, storybook-html: add header story for no site search, adjust CSS

--- packages/storybook-html/src/components/header/header.stories.js
+++ packages/storybook-html/src/components/header/header.stories.js
@@ -9,31 +9,37 @@ import headerCreate from '@uq-uidf/header/src/js/header.es6';
 
 // import HTML template strings
 import headerHTML from './header.html';
+import headerNoLocalSearchHTML from './header_no-local-search.html';
 
 import docs from './header.docs.mdx';
 
 export default {
-    title: 'Components/Header',
-    parameters: {
-      docs: {
-        page: docs
-      },
-      backgrounds: [
-        { name: 'UQ Neutral 1', value: '#D7D1CC', default: true }
-      ]
+  title: 'Components/Header',
+  parameters: {
+    docs: {
+      page: docs
     },
-    decorators: [expandedStory]
+    backgrounds: [
+      { name: 'UQ Neutral 1', value: '#D7D1CC', default: true }
+    ]
+  },
+  decorators: [expandedStory]
 };
 
 export const header = () => {
-    useEffect(() => {
-        new headerCreate();
-      });
-    return headerHTML;
+  useEffect(() => {
+    new headerCreate();
+  });
+  return headerHTML;
+};
+  
+export const headerNoLocalSearch = () => {
+  useEffect(() => {
+    new headerCreate();
+  });
+  return headerNoLocalSearchHTML;
 };
 
-header.story = {
-  name: "Header" 
+headerNoLocalSearch.story = {
+  name: "Header with global search only"
 }
-  
-

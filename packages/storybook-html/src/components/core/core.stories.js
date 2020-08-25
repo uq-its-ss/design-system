1063d461a6b026e56c42287d3dec218bff139793
uqmchu <m.chu@uq.edu.au>
2020-08-25 13:45:30 +1000

    Integrate body new package, remove Foundation global rendered SCSS
    Refactor done on: grid-menu, accordion, core

--- packages/storybook-html/src/components/core/core.stories.js
+++ packages/storybook-html/src/components/core/core.stories.js
@@ -30,7 +30,7 @@ colours.story = {
 export const fonts = () => {
   return `
     <div class="uidf-fonts">
-      <div class="uidf-font uidf-font--roboto uidf-font--300">Roboto Light 300</div>
+      <div class="uidf-font uidf-font--roboto uidf-font--300">Roboto Light 300 (deprecated)</div>
       <div class="uidf-font uidf-font--roboto">Roboto Regular 400</div>
       <div class="uidf-font uidf-font--roboto uidf-font--500">Roboto Medium 500</div>
       <div class="uidf-font uidf-font--merriweather uidf-font--italic">Merriweather Regular 400 italic</div>

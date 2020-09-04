d3289ca1dd4525558e3993b90de913d9c12c8e1c
uqmchu <m.chu@uq.edu.au>
2020-09-04 11:02:04 +1000

    storybook-html: fix progression stories JS

--- packages/storybook-html/src/patterns/progression/progression.stories.js
+++ packages/storybook-html/src/patterns/progression/progression.stories.js
@@ -1,18 +1,17 @@
-import { useEffect } from '@storybook/client-api';
-import expandedStory from '../../../.storybook/decorators/expanded.js';
-
 // import HTML template strings
 import progressionHTML from '../../components/progression/progression.html';
 
 export default {
     title: 'Patterns/Progression - Text, Step ',
-    decorators: [expandedStory]
+    parameters: {
+      docs: {
+        inlineStories: false
+      },
+      layout: 'padded'
+    }
 };
 
 export const example = () => {
-    useEffect(() => {
-
-      });
     return `
       <br/><br/>
       <div class="row">

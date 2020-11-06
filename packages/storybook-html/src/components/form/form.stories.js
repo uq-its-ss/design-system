c90e4a81f46883bea3166735bc953a9127eb60e2
uqmchu <m.chu@uq.edu.au>
2020-11-06 12:57:44 +1000

    storybook-html, body, table: tidy up table work for incl. in forms work

--- packages/storybook-html/src/components/form/form.stories.js
+++ packages/storybook-html/src/components/form/form.stories.js
@@ -3,7 +3,6 @@ import './style.scss';
 
 // import HTML template strings
 import allHTML from './all.html';
-import inputHTML from './input.html';
 import textAreaHTML from './text-area.html';
 import selectHTML from './select.html';
 
@@ -23,13 +22,34 @@ export default {
   }
 };
 
-export const kitchenSink = () => {
-  return allHTML;
-};
+export const inputText = () => `
+  <table>
+    <thead>
+      <tr>
+        <th>Default</th>
+        <th>Read-only</th>
+        <th>Disabled</th>
+      <tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <label>Text input</label>
+          <input type="text" />
+        </td>
+        <td>
+          <label>Text input <input type="text" readonly /></label>
+        </td>
+        <td>
+          <label>Text input</label>
+          <input type="text" disabled />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+`;
 
-export const input = () => {
-  return inputHTML;
-};
+inputText.storyName = "Text input";
 
 export const textArea = () => {
   return textAreaHTML;
@@ -37,4 +57,8 @@ export const textArea = () => {
 
 export const select = () => {
   return selectHTML;
+};
+
+export const kitchenSink = () => {
+  return allHTML;
 };

c6c2c66ee96fc5f65461e7553a1460adabd1c136
Bcrunk <b.crunkhorn@uq.edu.au>
2020-12-14 09:26:56 +1000

    Added H1 heading examples

--- packages/storybook-html/src/patterns/header-footer/header-footer.stories.js
+++ packages/storybook-html/src/patterns/header-footer/header-footer.stories.js
@@ -28,7 +28,9 @@ export const example = () => {
     return `
       ${headerHTML}
       ${siteHeaderHTML}
-      <br/><br/>
+      <div class="uq-grid">
+      <div class="uq-grid__col uq-grid__col--6">
+      <h1>Heading level 1 (h1)</h1>
       <div class="uq-grid uq-grid--golden">
         <div class="uq-grid__col">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit ut aliquam purus sit amet luctus venenatis. Ut tellus elementum sagittis vitae et leo. Eu sem integer vitae justo. Massa vitae tortor condimentum lacinia quis. Vel fringilla est ullamcorper eget nulla facilisi. Quis imperdiet massa tincidunt nunc. Convallis tellus id interdum velit laoreet id. Eu mi bibendum neque egestas congue quisque egestas diam in.</p>
@@ -37,7 +39,8 @@ export const example = () => {
           <p><small>Id diam vel quam elementum pulvinar. Vestibulum sed arcu non odio euismod lacinia. Facilisis sed odio morbi quis. Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.</small></p>
         </div>
       </div>
-      <br/><br/>
+      </div>
+      </div>
       ${footerHTML}
     `;
 };

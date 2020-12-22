5a9f35ca86afa51fb2703a48932cd2bf31f85734
Timmy George <timmy.george@uq.edu.au>
2020-12-22 17:37:38 +1000

    Card review restructure, name changes, add compact

--- packages/storybook-html/src/components/card/card.stories.js
+++ packages/storybook-html/src/components/card/card.stories.js
@@ -17,235 +17,179 @@ export default {
   }
 };
 
-export const defaultCard = () => {
+export const cardDefault = () => {
   return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <a class="card__link" href="#">
-              <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
-            </a>
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date" tabindex="0">Sunday 16 August, 2015</div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-              <div class="card__summary" tabindex="0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </div>
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <a class="card__link" href="#">
+            <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
+          </a>
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__summary">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
             </div>
           </div>
-          <div class="card__footer"></div>
         </div>
-      </div>
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <a class="card__link" href="#">
-              <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
-            </a>
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date" tabindex="0">Sunday 16 August, 2015</div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-              <div class="card__summary" tabindex="0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </div>
-            </div>
-          </div>
-          <div class="card__footer"></div>
-        </div>
-      </div>
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <a class="card__link" href="#">
-              <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
-            </a>
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date" tabindex="0">Sunday 16 August, 2015</div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-              <div class="card__summary" tabindex="0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </div>
-            </div>
-          </div>
-          <div class="card__footer"></div>
-        </div>
+        <div class="card__footer"></div>
       </div>
     </div>
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <a class="card__link" href="#">
+            <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
+          </a>
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__summary">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </div>
+          </div>
+        </div>
+        <div class="card__footer"></div>
+      </div>
+    </div>
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <a class="card__link" href="#">
+            <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
+          </a>
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__summary">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </div>
+          </div>
+        </div>
+        <div class="card__footer"></div>
+      </div>
+    </div>
+  </div>
   `;
 };
 
-defaultCard.storyName = 'Default card';
+cardDefault.storyName = 'Card Default';
 
-export const defaultCardWithLink = () => {
+export const cardWithLinks = () => {
   return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <a class="card__link" href="#">
-              <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')"></div>
-            </a>
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date" tabindex="0">Sunday 16 August, 2015</div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-              <div class="card__summary" tabindex="0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </div>
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <a class="card__link" href="#">
+            <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')"></div>
+          </a>
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__summary">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
             </div>
           </div>
-          <div class="card__footer">
-            <div class="uq-grid">
-              <div class="uq-grid__col uq-grid__col--3">
-                <a class="card__link" href="#">Action link A</a>
-              </div>
-              <div class="uq-grid__col uq-grid__col--3">
-                <a class="card__link" href="#">Action link B</a>
-              </div>
+        </div>
+        <div class="card__footer">
+          <div class="uq-grid">
+            <div class="uq-grid__col uq-grid__col--3">
+              <a class="card__link" href="#">Action link A</a>
+            </div>
+            <div class="uq-grid__col uq-grid__col--3">
+              <a class="card__link" href="#">Action link B</a>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   `;
 };
 
-defaultCardWithLink.storyName = 'Default card with link';
+cardWithLinks.storyName = 'Card with links';
 
+export const cardWithAdditionalText = () => {
+  return `
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <a class="card__link" href="#">
+            <div class="card__image-container" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg');"></div>
+          </a>
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__date">Sunday 16 August, 2015</div>
+            <div class="card__location">
+              <a class="card__link" href="#">Gatton campus</a>
+            </div>
+            <div class="card__summary">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </div>
+          </div>
+        </div>
+        <div class="card__footer"></div>
+      </div>
+    </div>
+  </div>
+  `;
+};
 
-
-
+cardWithAdditionalText.storyName = 'Card with additional text';
 
 export const overlayCardSquare = () => {
   return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <a class="card__primary-link" href="https://campuses.uq.edu.au/st-lucia">
-          <div class="entity entity-paragraphs-item paragraphs-item-card-custom card--square view-mode-card_overlay card card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9492/stlucia-card.jpg')">
-            <div class="card__header"></div>
-            <div class="card__body">
-              <div class="card__content">
-                <div class="card__title">
-                  <span tabindex="0">St Lucia</span>
-                </div>
-                <div class="card__summary" tabindex="0"></div>
-              </div>
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <a class="card__primary-link" href="https://campuses.uq.edu.au/st-lucia">
+        <div class="card card--square card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9492/stlucia-card.jpg')">
+          <div class="card__header"></div>
+          <div class="card__body">
+            <div class="card__content">
+              <div class="card__title">Title of item</div>
+              <div class="card__summary"></div>
             </div>
           </div>
-        </a>
-      </div>
+        </div>
+      </a>
     </div>
+  </div>
   `;
 };
 
 overlayCardSquare.storyName = 'Overlay card square';
 
-export const overlayCardSquareWithAdditionalText = () => {
-  return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card card--background-image card--square" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date">
-                <a class="card__link" href="#">Sunday 16 August, 2015</a>
-              </div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-            </div>
-          </div>
-          <div class="card__footer"></div>
-        </div>
-      </div>
-    </div>
-  `;
-};
-
-overlayCardSquareWithAdditionalText.storyName = 'Overlay card square with additional text';
-
-export const overlayCardSquareWithLink = () => {
-  return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
-        <div class="card card--background-image card--square" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date">
-                <a class="card__link" href="#">Sunday 16 August, 2015</a>
-              </div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-            </div>
-          </div>
-          <div class="card__footer">
-            <a class="card__link" href="#">Action link</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-};
-
-overlayCardSquareWithLink.storyName = 'Overlay card square with link';
-
-
-
 export const overlayCardPortrait = () => {
   return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6">
-        <div class="card card--background-image card--portrait" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date">
-                <a class="card__link" href="#">Sunday 16 August, 2015</a>
-              </div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-            </div>
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6">
+      <div class="card card--portrait card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
           </div>
         </div>
       </div>
     </div>
+  </div>
   `;
 };
 
@@ -253,27 +197,94 @@ overlayCardPortrait.storyName = 'Overlay card portrait';
 
 export const overlayCardLandscape = () => {
   return `
-    <div class="uq-grid">
-      <div class="uq-grid__col uq-grid__col--6">
-        <div class="card card--background-image card--landscape" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
-          <div class="card__header"></div>
-          <div class="card__body">
-            <div class="card__content">
-              <h2 class="card__title">
-                <a class="card__link" href="#">Title of item</a>
-              </h2>
-              <div class="card__date">
-                <a class="card__link" href="#">Sunday 16 August, 2015</a>
-              </div>
-              <div class="card__location">
-                <a class="card__link" href="#">Gatton campus</a>
-              </div>
-            </div>
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6">
+      <div class="card card--landscape card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
           </div>
         </div>
       </div>
     </div>
+  </div>
   `;
 };
 
 overlayCardLandscape.storyName = 'Overlay card landscape';
+
+export const overlayCardCompact = () => {
+  return `
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6">
+      <div class="card card--compact card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+};
+
+overlayCardCompact.storyName = 'Overlay card compact';
+
+export const overlayCardSquareWithLink = () => {
+  return `
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card card--square card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+          </div>
+        </div>
+        <div class="card__footer">
+          <a class="card__link" href="#">Action link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+};
+
+overlayCardSquareWithLink.storyName = 'Overlay card square with link';
+
+export const overlayCardSquareWithAdditionalText = () => {
+  return `
+  <div class="uq-grid">
+    <div class="uq-grid__col uq-grid__col--6 uq-grid__col--md-4 uq-grid__col--xl-4">
+      <div class="card card--square card--background-image" style="background-image: url('https://campuses.uq.edu.au/files/9749/gatton-card2.jpg')">
+        <div class="card__header"></div>
+        <div class="card__body">
+          <div class="card__content">
+            <h2 class="card__title">
+              <a class="card__link" href="#">Title of item</a>
+            </h2>
+            <div class="card__date">
+              <a class="card__link" href="#">Sunday 16 August, 2015</a>
+            </div>
+            <div class="card__location">
+              <a class="card__link" href="#">Gatton campus</a>
+            </div>
+          </div>
+        </div>
+        <div class="card__footer"></div>
+      </div>
+    </div>
+  </div>
+  `;
+};
+
+overlayCardSquareWithAdditionalText.storyName = 'Overlay card square with additional text';

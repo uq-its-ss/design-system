3c4d403bb78dea3f14d527e6dacae99b237d2d19
Timmy George <timmy.george@uq.edu.au>
2021-02-04 13:32:32 +1000

    default tab implementation initial commit

--- packages/storybook-html/src/components/tabs/tabs.stories.js
+++ packages/storybook-html/src/components/tabs/tabs.stories.js
@@ -4,9 +4,15 @@ import './tabs.scss';
 // import HTML template strings
 import tabsHTML from './tabs.html';
 
+// documentation
+//import docs from './tabs.docs.mdx';
+
 export default {
   title: 'Components/Tabs',
   parameters: {
+    /*docs: {
+      page: docs
+    },*/
     layout: "padded",
     previewTabs: {
       canvas: {hidden: false}
@@ -17,3 +23,26 @@ export default {
 export const all = () => {
   return tabsHTML;
 };
+
+export const tabsDefault = () => {
+  return `
+<div class="tabs tabs--bordered">
+  <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
+    <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
+    <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
+    <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab3-panel" id="tab3" tabindex="-1">Tab 3</button>
+  </div>
+  <div class="tabs__tab-panel tabs__tab-panel--active" role="tabpanel" id="tab1-panel" aria-labelledby="tab1" tabindex="0">
+    <p>Tab1 panel text</p>
+  </div>
+  <div class="tabs__tab-panel" role="tabpanel" id="tab2-panel" aria-labelledby="tab2" tabindex="0" hidden="">
+    <p>Tab2 panel text</p>
+  </div>
+  <div class="tabs__tab-panel" role="tabpanel" id="tab3-panel" aria-labelledby="tab3" tabindex="0" hidden="">
+    <p>Tab3 panel text</p>
+  </div>
+</div>
+  `;
+};
+
+tabsDefault.storyName = 'Tabs Default';

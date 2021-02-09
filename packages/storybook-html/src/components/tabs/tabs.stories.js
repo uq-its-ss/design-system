bacee60f9ed195913b7aed1cbcb0801a01b8f8a2
Timmy George <timmy.george@uq.edu.au>
2021-02-09 14:12:26 +1000

    with icon variant added

--- packages/storybook-html/src/components/tabs/tabs.stories.js
+++ packages/storybook-html/src/components/tabs/tabs.stories.js
@@ -93,6 +93,29 @@ export const tabsBoxed = () => {
   return `
   <div class="tabs tabs--boxed">
     <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
+      <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab3-panel" id="tab3" tabindex="-1">Tab 3</button>
+    </div>
+    <div class="tabs__tab-panel tabs__tab-panel--active" role="tabpanel" id="tab1-panel" aria-labelledby="tab1" tabindex="0">
+      <p>Tab1 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab2-panel" aria-labelledby="tab2" tabindex="0" hidden="">
+      <p>Tab2 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab3-panel" aria-labelledby="tab3" tabindex="0" hidden="">
+      <p>Tab3 panel text</p>
+    </div>
+  </div>
+  `;
+};
+
+tabsBoxed.storyName = 'Boxed';
+
+export const tabsWithIcon = () => {
+  return `
+  <div class="tabs tabs--outline">
+    <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
       <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">
         <span class="uq-icon uq-icon--pin"></span> Tab 1
       </button>
@@ -116,7 +139,7 @@ export const tabsBoxed = () => {
   `;
 };
 
-tabsBoxed.storyName = 'Boxed';
+tabsWithIcon.storyName = 'With icon';
 
 export const tabsFullWidth = () => {
   return `

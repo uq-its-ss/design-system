18b2678df03d67efa1263025fa54523133c966c8
Timmy George <timmy.george@uq.edu.au>
2021-02-09 10:01:05 +1000

    Variants

--- packages/storybook-html/src/components/tabs/tabs.stories.js
+++ packages/storybook-html/src/components/tabs/tabs.stories.js
@@ -27,7 +27,7 @@ export default {
   return tabsHTML;
 };*/
 
-export const tabsSimple = () => {
+export const tabsMinimal = () => {
   return `
   <div class="tabs">
     <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
@@ -48,11 +48,11 @@ export const tabsSimple = () => {
   `;
 };
 
-tabsSimple.storyName = 'Simple';
+tabsMinimal.storyName = 'Minimal';
 
-export const tabsBordered = () => {
+export const tabsLined = () => {
   return `
-  <div class="tabs tabs--bordered">
+  <div class="tabs tabs--lined">
     <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
       <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
       <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
@@ -71,11 +71,11 @@ export const tabsBordered = () => {
   `;
 };
 
-tabsBordered.storyName = 'Bordered';
+tabsLined.storyName = 'Lined';
 
-export const tabsBorderedMinimal = () => {
+export const tabsOutline = () => {
   return `
-  <div class="tabs tabs--bordered-minimal">
+  <div class="tabs tabs--outline">
     <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
       <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
       <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
@@ -94,11 +94,34 @@ export const tabsBorderedMinimal = () => {
   `;
 };
 
-tabsBorderedMinimal.storyName = 'Bordered minimal';
+tabsOutline.storyName = 'Outline';
+
+export const tabsBoxed = () => {
+  return `
+  <div class="tabs tabs--boxed">
+    <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
+      <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab3-panel" id="tab3" tabindex="-1">Tab 3</button>
+    </div>
+    <div class="tabs__tab-panel tabs__tab-panel--active" role="tabpanel" id="tab1-panel" aria-labelledby="tab1" tabindex="0">
+      <p>Tab1 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab2-panel" aria-labelledby="tab2" tabindex="0" hidden="">
+      <p>Tab2 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab3-panel" aria-labelledby="tab3" tabindex="0" hidden="">
+      <p>Tab3 panel text</p>
+    </div>
+  </div>
+  `;
+};
+
+tabsBoxed.storyName = 'Boxed';
 
 export const tabsFullWidth = () => {
   return `
-  <div class="tabs tabs--bordered tabs--full-width">
+  <div class="tabs tabs--boxed tabs--full-width">
     <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
       <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
       <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
@@ -118,3 +141,26 @@ export const tabsFullWidth = () => {
 };
 
 tabsFullWidth.storyName = 'Full width';
+
+export const tabsCompact = () => {
+  return `
+  <div class="tabs tabs--outline tabs--compact">
+    <div class="tabs__tab-list" role="tablist" aria-label="Tab List">
+      <button class="tabs__tab tabs__tab--active" role="tab" aria-selected="true" aria-controls="tab1-panel" id="tab1">Tab 1</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab2-panel" id="tab2" tabindex="-1">Tab 2</button>
+      <button class="tabs__tab" role="tab" aria-selected="false" aria-controls="tab3-panel" id="tab3" tabindex="-1">Tab 3</button>
+    </div>
+    <div class="tabs__tab-panel tabs__tab-panel--active" role="tabpanel" id="tab1-panel" aria-labelledby="tab1" tabindex="0">
+      <p>Tab1 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab2-panel" aria-labelledby="tab2" tabindex="0" hidden="">
+      <p>Tab2 panel text</p>
+    </div>
+    <div class="tabs__tab-panel" role="tabpanel" id="tab3-panel" aria-labelledby="tab3" tabindex="0" hidden="">
+      <p>Tab3 panel text</p>
+    </div>
+  </div>
+  `;
+};
+
+tabsCompact.storyName = 'Compact';

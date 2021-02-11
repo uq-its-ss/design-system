7ac30375d4202cc5c08685ae47124ddd7fea9888
uqmchu <m.chu@uq.edu.au>
2021-02-11 12:26:05 +1000

    tabs, storybook-html: flawed implementation of tabs JS
    JS needs to be converted to constructor pattern.

--- packages/storybook-html/src/components/tabs/tabs.stories.js
+++ packages/storybook-html/src/components/tabs/tabs.stories.js
@@ -24,7 +24,7 @@ export default {
 
 export const tabsOutline = () => {
   useEffect(() => {
-    tabs(); 
+    tabs();
   });
   return `
   <div class="tabs tabs--outline">
--- packages/tabs/src/js/tabs.es6.js
+++ packages/tabs/src/js/tabs.es6.js
@@ -170,13 +170,13 @@ const tabs = function () {
   
     // Deactivate all tabs and tab panels
     function deactivateTabs () {
-      for (t = 0; t < tabs.length; t++) {
+      for (let t = 0; t < tabs.length; t++) {
         tabs[t].setAttribute('tabindex', '-1');
         tabs[t].setAttribute('aria-selected', 'false');
         tabs[t].removeEventListener('focus', focusEventHandler);
       };
   
-      for (p = 0; p < panels.length; p++) {
+      for (let p = 0; p < panels.length; p++) {
         panels[p].setAttribute('hidden', 'hidden');
       };
     };
@@ -259,3 +259,4 @@ const tabs = function () {
   };
 
   export {tabs as default};
+  

1ed04d1531da92ecfebcad4233ec94640257136a
Bcrunk <github@crunkhorn.com.au>
2021-07-08 10:42:09 +1000

    updates

--- packages/storybook-html/src/components/blockquote/blockquote.stories.js
+++ packages/storybook-html/src/components/blockquote/blockquote.stories.js
@@ -3,16 +3,44 @@ import './blockquote.scss';
 
 // import HTML template strings
 import blockquoteHTML from './blockquote.html';
+import blockquoteDarkHTML from './blockquote_dark.html';
+import blockquoteBoxedHTML from './blockquote_boxed.html';
+
+// documentation
+import docs from './blockquote.docs.mdx';
 
 export default {
   title: 'Components/Blockquote',
   parameters: {
+    docs: {
+      page: docs
+    },
+    layout: "padded",
     previewTabs: {
       canvas: {hidden: false}
     }
   }
 };
 
-export const blockquote = () => {
+export const blockquoteDefault = () => {
   return blockquoteHTML;
 };
+blockquoteDefault.storyName = 'blockquote';
+
+export const blockquoteDark = () => {
+  return blockquoteDarkHTML;
+};
+blockquoteDark.storyName = 'Dark';
+blockquoteDark.parameters = {
+  docs: {
+     inlineStories: false
+  },
+  backgrounds: {
+    default: 'Dark'
+  } 
+};
+
+export const blockquoteBoxed = () => {
+  return blockquoteBoxedHTML;
+};
+blockquoteBoxed.storyName = 'Boxed';

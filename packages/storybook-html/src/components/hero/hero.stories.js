71d7fcc4f675f84b226cae05446bb82b49c26c3a
Nate Millington <n.millington@uq.edu.au>
2021-08-11 10:09:07 +1000

    added tall variant

--- packages/storybook-html/src/components/hero/hero.stories.js
+++ packages/storybook-html/src/components/hero/hero.stories.js
@@ -59,6 +59,30 @@ export const FeatureHero = () => {
 
 FeatureHero.storyName = 'Feature image hero';
 
+export const TallHero = () => {
+  return `
+<div class="uq-hero uq-hero--feature-image uq-hero--tall">  
+  <div class="uq-hero__image">
+    <div class="uq-hero__image--small">
+      <img src="/images/hero/hero-example--small.jpg" />
+    </div>
+    <div class="uq-hero__image--large">
+      <img src="/images/hero/hero-example--large.jpg" />
+    </div>
+  </div>
+  <div class="uq-hero__content">
+    <div class="uq-hero__text">
+      <h1>Start postgraduate study online</h1>
+      <p class="uq-hero__subtext">Apply by June 30</p>
+      <p><a class="uq-button" href="#">Learn more</a></p>
+    </div>
+  </div>
+</div>
+  `;
+};
+
+TallHero.storyName = 'Tall hero';
+
 // export const hero = () => {
 //   return heroHTML;
 // };

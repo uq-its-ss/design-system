1ae6dac33911b78faec4990402abeb321856d9b3
Jen Lui <j.lui@uq.edu.au>
2021-09-13 13:45:55 +1000

    uplift and standardise docs and add variants

--- packages/storybook-html/src/components/loading-spinner/loading-spinner.stories.js
+++ packages/storybook-html/src/components/loading-spinner/loading-spinner.stories.js
@@ -3,6 +3,8 @@ import './loading-spinner.scss';
 
 // import HTML template strings
 import loadingSpinnerHTML from './loading-spinner.html';
+import loadingSpinnerLightHTML from './loading-spinner-light.html';
+import loadingSpinnerSmallHTML from './loading-spinner-small.html';
 
 // documentation
 import docs from './loading-spinner.docs.mdx';
@@ -24,4 +26,30 @@ export const loadingSpinner = () => {
   return loadingSpinnerHTML;
 };
 
-loadingSpinner.storyName = 'Loading spinner';
+loadingSpinner.storyName = 'Default';
+
+export const loadingSpinnerLight = () => {
+  return loadingSpinnerLightHTML;
+}
+
+loadingSpinnerLight.storyName = 'Light';
+loadingSpinnerLight.parameters = {
+  docs: {
+    inlineStories: false
+  },
+  backgrounds: {
+    default: 'Purple',
+    values: [
+      {
+        name: 'Purple',
+        value: '#51247A',
+      }
+    ]
+  }
+};
+
+export const loadingSpinnerSmall = () => {
+  return loadingSpinnerSmallHTML;
+}
+
+loadingSpinnerSmall.storyName = 'Small';

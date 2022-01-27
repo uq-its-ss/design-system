73d565db2addf11417d3ed54b394498670543479
Nate Millington <n.millington@uq.edu.au>
2022-01-27 13:51:42 +1000

    header js cleanup, megamenu styling tweaks

--- packages/header/src/js/header.js
+++ packages/header/src/js/header.js
@@ -13,21 +13,24 @@ class header {
   }
 
   init() {
-    // this.toggle = document.querySelector('.uq-header__search-toggle-button');
-    // this.search = document.querySelector('.uq-header__search');
-    // this.searchInput = document.querySelector('.uq-header__search-query-input');
-    // this.meta = document.querySelector('meta.uq-header__mq--desktop');
-    // this.navigation = document.querySelector('.uq-header__navigation');
+    this.toggle = document.querySelector('.uq-header__search-toggle-button');
+    this.search = document.querySelector('.uq-header__search');
+    this.searchInput = document.querySelector('.uq-header__search-query-input');
+    this.meta = document.querySelector('meta.uq-header__mq--desktop');
+    this.navigation = document.querySelector('.uq-header__navigation');
 
-    // if (this.meta == null) {
-    //   this.meta = document.createElement('meta');
-    //   this.meta.classList.add('uq-header__mq--desktop');
-    //   document.head.appendChild(this.meta);
-    // };
+    if (this.meta == null) {
+      this.meta = document.createElement('meta');
+      this.meta.classList.add('uq-header__mq--desktop');
+      document.head.appendChild(this.meta);
+    };
 
-    // this.mqLarge = window.getComputedStyle(this.meta).getPropertyValue('font-family');
-    // this.mqLarge = this.mqLarge.trim().slice(1, -1); // browsers re-quote string style values
-    // this.mqLargeList = window.matchMedia(this.mqLarge);
+    this.mqLarge = window.getComputedStyle(this.meta).getPropertyValue('font-family');
+    this.mqLarge = this.mqLarge.trim().slice(1, -1); // browsers re-quote string style values
+    this.mqLargeList = window.matchMedia(this.mqLarge);
+
+    console.log('init');
+
 
     this.searchToggle = document.querySelector('.main-nav__search-toggle');
     this.searchBlock = document.querySelector('.new-header__search');
@@ -40,28 +43,23 @@ class header {
 
 
 
-    console.log('init');
-
-
 
   }
 
-  // handleToggle() {
-  //   console.log('click');
-  //   this.megamenu.classList.toggle('new-header__megamenu--open');
-  //   // this.search.classList.toggle('uq-header__search--open');
-  //   // this.navigation.classList.toggle('uq-header__navigation--open');
+  handleToggle() {
+    this.search.classList.toggle('uq-header__search--open');
+    this.navigation.classList.toggle('uq-header__navigation--open');
 
-  //   // if (this.search.classList.contains('uq-header__search--open')) {
-  //   //   if (this.mqLargeList.matches) {
-  //   //     this.searchInput.focus();
-  //   //   } else {
-  //   //     this.toggle.blur();
-  //   //   }
-  //   // } else {
-  //   //   this.toggle.blur();
-  //   // }
-  // }
+    if (this.search.classList.contains('uq-header__search--open')) {
+      if (this.mqLargeList.matches) {
+        this.searchInput.focus();
+      } else {
+        this.toggle.blur();
+      }
+    } else {
+      this.toggle.blur();
+    }
+  }
 };
 
 export {header as default};

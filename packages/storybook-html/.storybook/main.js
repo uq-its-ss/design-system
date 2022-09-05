1325b408dff4b5807f8b1ff63effc526afccdf35
uqbcrunk <b.crunkhorn@uq.edu.au>
2022-09-05 11:15:03 +1000

    Linted

--- packages/storybook-html/.storybook/main.js
+++ packages/storybook-html/.storybook/main.js
@@ -5,7 +5,11 @@ module.exports = {
     "../stories/**/*.@(stories|story).mdx",
     "../stories/**/*.@(stories|story).@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-a11y", "@storybook/addon-essentials", "storybook-addon-designs"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    "storybook-addon-designs",
+  ],
   staticDirs: ["../public"],
   features: {
     postcss: false,
--- packages/storybook-html/README.md
+++ packages/storybook-html/README.md
@@ -2,10 +2,9 @@
 
 > Component-driven development (CDD) using Storybook for HTML as a style-guide.
 
-
 # Embed Figma in Storybook with the addon
 
-References to the FIGMA design files, to improve design and development workflow. 
+References to the FIGMA design files, to improve design and development workflow.
 
 devDependencies added `storybook-addon-designs`
 

f1b7df2002e773b3d7e1c4fec3ad32a684659c8f
Nate Millington <n.millington@uq.edu.au>
2023-04-18 10:26:56 +1000

    changed to 3 trailing breadcrumbs. remove console.log

--- packages/breadcrumb/src/js/breadcrumb.js
+++ packages/breadcrumb/src/js/breadcrumb.js
@@ -7,31 +7,27 @@
 
 class Breadcrumb {
   constructor(el) {
-
     this.init(el);
-
   }
 
   init(el) {
-    
-    const breadcrumbs = el.querySelectorAll('.uq-breadcrumb__item');
-    console.log(breadcrumbs);
+    const breadcrumbs = el.querySelectorAll(".uq-breadcrumb__item");
 
     // truncate if more than 4 options
     if (breadcrumbs.length > 4) {
       var crumbs = Array.from(breadcrumbs);
-       
+
       // create the expand crumb link
-      var expandCrumb = document.createElement('li');
-      expandCrumb.classList.add('uq-breadcrumb__item', 'uq-breadcrumb__expand');
-      expandCrumb.innerHTML  = '<a class="uq-breadcrumb__link">...</a>';
-       
+      var expandCrumb = document.createElement("li");
+      expandCrumb.classList.add("uq-breadcrumb__item", "uq-breadcrumb__expand");
+      expandCrumb.innerHTML = '<a class="uq-breadcrumb__link">...</a>';
+
       // add expand crumb after the home crumb
-      var firstCrumb =  el.querySelector('.uq-breadcrumb__item:first-of-type');  
+      var firstCrumb = el.querySelector(".uq-breadcrumb__item:first-of-type");
       firstCrumb.parentNode.insertBefore(expandCrumb, firstCrumb.nextSibling);
 
       // hide the crumbs in between
-      var hiddenCrumbs = crumbs.slice(1, crumbs.length - 2);
+      var hiddenCrumbs = crumbs.slice(1, crumbs.length - 3);
       hiddenCrumbs.forEach((crumb) => {
         crumb.classList.add("uq-breadcrumb__item--hidden");
       });
@@ -45,13 +41,8 @@ class Breadcrumb {
         });
         expandCrumb.classList.toggle("uq-breadcrumb__item--hidden");
       }
-
     }
-    
   }
-  
-  
-
 }
 
 export default Breadcrumb;

afd8f09c1776c0c0ed480abdedc75e2a28597dee
Nate Millington <n.millington@uq.edu.au>
2023-10-16 08:30:15 +1000

    breadcrumb story setup

--- packages/storybook-html/stories/components/breadcrumb/breadcrumb.stories.js
+++ packages/storybook-html/stories/components/breadcrumb/breadcrumb.stories.js
@@ -1,16 +1,12 @@
+// documentation
+import docs from "./breadcrumb.docs.mdx";
+import breadcrumbTwig from "./breadcrumb.twig";
 import { useEffect } from "@storybook/addons";
 
 // import styles
-import "./breadcrumb.scss";
-
-// import HTML template strings
-import breadcrumbHTML from "./breadcrumb.html";
-
+import "@uqds/breadcrumb/src/scss/_component.scss";
 // import scripts
-import breadcrumbCreate from "@uqds/breadcrumb/src/js/breadcrumb";
-
-// documentation
-import docs from "./breadcrumb.docs.mdx";
+import breadcrumbCreate from "@uqds/breadcrumb/src/js/breadcrumb.js";
 
 export default {
   title: "Components/Breadcrumb",
@@ -26,57 +22,34 @@ export default {
 
 export const breadcrumb = () => {
   useEffect(() => {
-    const breadcrumbDefault = document.querySelector(".uq-breadcrumb");
-    new breadcrumbCreate(breadcrumbDefault);
+    const breadcrumbElem = document.querySelector(".uq-breadcrumb");
+    new breadcrumbCreate(breadcrumbElem);
   });
-  return breadcrumbHTML;
-};
-
-breadcrumb.storyName = "Default";
-
-export const breadcrumbTruncation = () => {
-  useEffect(() => {
-    const breadcrumbTrunc = document.querySelector(".uq-breadcrumb--truncated");
-    new breadcrumbCreate(breadcrumbTrunc);
+  return breadcrumbTwig({
+    breadcrumbs: [
+      {
+        text: "Home",
+        url: "https://www.uq.edu.au/",
+      },
+      {
+        text: "Study",
+        url: "/",
+      },
+      {
+        text: "Study options",
+        url: "/study-options",
+      },
+      {
+        text: "Find a program",
+        url: "/study-options/programs",
+      },
+      {
+        text: "Bachelors of Computer Science / Business Management",
+        url: "/study-options/programs/bachelors-computer-science-business-management-2463",
+      },
+      {
+        text: "Entry requirements",
+      },
+    ],
   });
-  return `
-<nav class="uq-breadcrumb uq-breadcrumb--truncated" aria-label="breadcrumb">
-      <ol class="uq-breadcrumb__list" itemscope="" itemtype="https://schema.org/BreadcrumbList">
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" title="Home" href="/">Home</a>
-          <meta itemprop="position" content="1">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="Level one" href="/information-and-services">Level one</a>
-          <meta itemprop="position" content="2">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="Level two" href="/information-and-services/internet-and-wifi">Level two</a>
-          <meta itemprop="position" content="3">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="A really long page title" href="#">A really long page title</a>
-          <meta itemprop="position" content="5">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="Another really long title" href="#">Another really long title</a>
-          <meta itemprop="position" content="6">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="Bachelor of Business with a Major in Management or Finance" href="#">Bachelor of Business with a Major in Management or Finance</a>
-          <meta itemprop="position" content="7">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          <a class="uq-breadcrumb__link" itemprop="item" title="Major Leadership and Management Science" href="#">Major Leadership and Management Science</a>
-          <meta itemprop="position" content="8">
-        </li>
-        <li class="uq-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-          Welcome to the Kitchen Sink page
-          <meta itemprop="position" content="9">
-        </li>
-      </ol>
-    </nav>
-  `;
 };
-
-breadcrumbTruncation.storyName = "Truncation";

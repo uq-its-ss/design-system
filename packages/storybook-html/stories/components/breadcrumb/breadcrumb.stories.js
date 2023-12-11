522126d51eb99ba746de7de49b8122a0cfc976f9
Nate Millington <n.millington@uq.edu.au>
2023-12-11 11:35:53 +1000

    remove truncation breadcrumb story and screenshot reference

--- packages/storybook-html/stories/components/breadcrumb/breadcrumb.stories.js
+++ packages/storybook-html/stories/components/breadcrumb/breadcrumb.stories.js
@@ -33,36 +33,3 @@ export const breadcrumb = () => {
 };
 
 breadcrumb.storyName = "Default";
-
-export const breadcrumbTruncation = () => {
-  useEffect(() => {
-    const breadcrumbTrunc = document.querySelector(".uq-breadcrumb--truncated");
-    new breadcrumbCreate(breadcrumbTrunc);
-  });
-  return `
-<nav class="uq-breadcrumb" aria-label="breadcrumb">
-      <ol class="uq-breadcrumb__list">
-        <li class="uq-breadcrumb__item">
-          <a class="uq-breadcrumb__link" title="Home" href="/">Home</a>
-        </li>
-        <li class="uq-breadcrumb__item">
-          <a class="uq-breadcrumb__link" title="Level one" href="/information-and-services">Level one</a>
-        </li>
-        <li class="uq-breadcrumb__item">
-          <a class="uq-breadcrumb__link" title="Level two" href="/information-and-services/internet-and-wifi">Level two</a>
-        </li>
-        <li class="uq-breadcrumb__item">
-          <a class="uq-breadcrumb__link" title="Bachelor of Business with a Major in Management or Finance" href="#">Bachelor of Business with a Major in Management or Finance</a>
-        </li>
-        <li class="uq-breadcrumb__item">
-          <a class="uq-breadcrumb__link" title="Major Leadership and Management Science" href="#">Major Leadership and Management Science</a>
-        </li>
-        <li class="uq-breadcrumb__item">
-          Current page
-        </li>
-      </ol>
-    </nav>
-  `;
-};
-
-breadcrumbTruncation.storyName = "Truncation";

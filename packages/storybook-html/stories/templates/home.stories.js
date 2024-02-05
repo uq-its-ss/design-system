e60a8dc4948d82ed99f65dd8b62826a096ae7694
jptaranto <jacktaranto@gmail.com>
2024-02-05 14:05:29 +1100

    chore: add shaded section back in

--- packages/storybook-html/stories/templates/home.stories.js
+++ packages/storybook-html/stories/templates/home.stories.js
@@ -10,14 +10,14 @@ export default {
     layout: "fullscreen",
     previewTabs: {
       canvas: { hidden: false },
-      'storybook/docs/panel': { hidden: true, }
+      "storybook/docs/panel": { hidden: true },
     },
-    viewMode: 'story',
+    viewMode: "story",
   },
   argTypes: {
     showGrid: {
       control: "boolean",
-    }
+    },
   },
   args: {
     showGrid: false,
@@ -26,29 +26,60 @@ export default {
 
 export const Home = {
   render: ({ showGrid }) => `
-<div class="${classNames({ "uqds-layout-demo-bg": showGrid } )}">
+<div class="${classNames({ "uqds-layout-demo-bg": showGrid })}">
   ${Header()}
   ${breadcrumb()}
   <div class="uq-sections">
     ${Hero.render(Hero.args)}
-    <div class="uq-container">
-      <div class="uq-sections">
-        <div class="uqds-layout-demo-el"></div> 
+    <div class="uq-section">
+      <div class="uq-container">
         <div class="uq-card-grid uq-card-grid--target-1x">
           <div class="uqds-layout-demo-card"></div>
           <div class="uqds-layout-demo-card"></div>
         </div>
+      </div>
+    </div>
+    <div class="uq-section">
+      <div class="uq-container">
         <div class="uq-card-grid uq-card-grid--target-2x">
           <div class="uqds-layout-demo-card"></div>
           <div class="uqds-layout-demo-card"></div>
         </div>
-        <div class="uqds-layout-demo-el"></div>
+      </div>
+    </div>
+    <div class="uq-section uq-section--shaded">
+      <div class="uq-container">
+        <div class="uq-section__header">
+          <h2 class="uq-section__title">Title</h2>
+          <div class="uq-section__description">
+            <p>Description</p>
+          </div>
+        </div>
+        <div class="uqds-layout-demo-el"></div> 
+      </div>
+    </div>
+    <div class="uq-section">
+      <div class="uq-container">
+        <div class="uqds-layout-demo-el"></div> 
+      </div>
+    </div>
+    <div class="uq-section uq-section--shaded">
+      <div class="uq-container">
+        <div class="uq-section__header">
+          <h2 class="uq-section__title">Title</h2>
+          <div class="uq-section__description">
+            <p>Description</p>
+          </div>
+        </div>
         <div class="uq-card-grid uq-card-grid--target-3x">
           <div class="uqds-layout-demo-card"></div>
           <div class="uqds-layout-demo-card"></div>
-          <div class="uqds-layout-demo-card"></div>
+          <div class="uqds-layout-demo-card"></div>  
         </div>
-        <div class="uqds-layout-demo-el"></div> 
+      </div>
+    </div>
+    <div class="uq-section">
+      <div class="uq-container">
         <div class="uq-card-grid uq-card-grid--target-4x">
           <div class="uqds-layout-demo-card"></div>
           <div class="uqds-layout-demo-card"></div>
@@ -60,4 +91,4 @@ export const Home = {
   </div>
   ${footer()}
 </div>`,
-}
+};

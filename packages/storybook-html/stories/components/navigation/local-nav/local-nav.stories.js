56ca5558ba01ce09accd440032e98643ad6f688b
jptaranto <jacktaranto@gmail.com>
2024-02-14 10:05:31 +1100

    chore: pr feedback

--- packages/storybook-html/stories/components/navigation/local-nav/local-nav.stories.js
+++ packages/storybook-html/stories/components/navigation/local-nav/local-nav.stories.js
@@ -18,24 +18,24 @@ export const LocalNav = {
   name: "Local nav",
   render: () => `
 <nav class="uq-local-nav" aria-label="Local navigation">
-  <div class="uq-local-nav__menu-item uq-local-nav__grandparent"><a href="#"><span class="uq-sr-only">Back to </span>UQ home</a></div>
-  <div class="uq-local-nav__menu-item uq-local-nav__parent"><a href="#"><span class="uq-sr-only">Back to </span>Research</a></div>
-  <ul class="uq-local-nav__menu">
-    <li class="uq-local-nav__menu-item"><a href="#">Research impact</a></li>
-    <li class="uq-local-nav__menu-item uq-local-nav--has-children uq-local-nav--is-current uq-local-nav--is-active">
-      <a href="#" aria-current="page">Research capabilities and initiatives</a>
-      <ul class="uq-local-nav__child-menu">
-        <li class="uq-local-nav__menu-item uq-local-nav--has-children uq-local-nav__child"><a href="#">Energy network</a></li>
-        <li class="uq-local-nav__menu-item uq-local-nav__child"><a href="#">Other networks</a></li>
+  <div class="uq-local-nav__grandparent"><a class="uq-local-nav__link" href="#"><span class="uq-sr-only">Back to </span>UQ home</a></div>
+  <div class="uq-local-nav__parent"><a class="uq-local-nav__link" href="#"><span class="uq-sr-only">Back to </span>Research</a></div>
+  <ul class="uq-local-nav__children">
+    <li class="uq-local-nav__child"><a class="uq-local-nav__link" href="#">Research impact</a></li>
+    <li class="uq-local-nav__child uq-local-nav--has-children uq-local-nav--current-child">
+      <a class="uq-local-nav__link uq-local-nav--active-link" href="#" aria-current="page">Research capabilities and initiatives</a>
+      <ul class="uq-local-nav__grandchildren">
+        <li class="uq-local-nav__grandchild uq-local-nav--has-children"><a class="uq-local-nav__link" href="#">Energy network</a></li>
+        <li class="uq-local-nav__grandchild"><a class="uq-local-nav__link" href="#">Other networks</a></li>
       </ul>
     </li>
-    <li class="uq-local-nav__menu-item"><a href="#">Ethics and integrity</a></li>
-    <li class="uq-local-nav__menu-item uq-local-nav--has-children"><a href="#">Graduate research</a></li>
-    <li class="uq-local-nav__menu-item uq-local-nav--has-children"><a href="#">Research partnerships</a></li>
-    <li class="uq-local-nav__menu-item"><a href="#">News</a></li>
-    <li class="uq-local-nav__menu-item uq-local-nav--has-children"><a href="#">Research and innovation week</a></li>
-    <li class="uq-local-nav__menu-item"><a href="#">Supporting our researchers</a></li>
-    <li class="uq-local-nav__menu-item"><a href="#">Contact</a></li>
+    <li class="uq-local-nav__child"><a class="uq-local-nav__link" href="#">Ethics and integrity</a></li>
+    <li class="uq-local-nav__child uq-local-nav--has-children"><a class="uq-local-nav__link" href="#">Graduate research</a></li>
+    <li class="uq-local-nav__child uq-local-nav--has-children"><a class="uq-local-nav__link" href="#">Research partnerships</a></li>
+    <li class="uq-local-nav__child"><a class="uq-local-nav__link" href="#">News</a></li>
+    <li class="uq-local-nav__child uq-local-nav--has-children"><a class="uq-local-nav__link" href="#">Research and innovation week</a></li>
+    <li class="uq-local-nav__child"><a class="uq-local-nav__link" href="#">Supporting our researchers</a></li>
+    <li class="uq-local-nav__child"><a class="uq-local-nav__link" href="#">Contact</a></li>
   </ul>
 </nav>
 `,

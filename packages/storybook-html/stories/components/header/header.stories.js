bf89aa4d4f61edb3d9a5861d38f19783d62a0fd6
Nate Millington <n.millington@uq.edu.au>
2024-04-15 13:08:40 +1000

    fix: remove DOMcontentload event listener and adjust js

--- packages/storybook-html/stories/components/header/header.stories.js
+++ packages/storybook-html/stories/components/header/header.stories.js
@@ -31,47 +31,48 @@ export const HeaderInterim = {
   play: ({ canvasElement }) => {
     const headerElem = canvasElement.querySelector(".uq-header");
     if (headerElem) new header(headerElem);
+    console.log("PRE DOM LOADED");
 
-    document.addEventListener("DOMContentLoaded", function () {
-      const menuLeftElem = document.getElementById("global-mobile-nav");
+    console.log("DOM LOADED");
+    const menuLeftElem = document.getElementById("global-mobile-nav");
 
-      const menuLeft = new SlideMenu(menuLeftElem, {
-        position: "left",
-        submenuLinkAfter: " ",
-        backLinkBefore: " ",
-      });
+    const menuLeft = new SlideMenu(menuLeftElem, {
+      position: "left",
+      submenuLinkAfter: " ",
+      backLinkBefore: " ",
+    });
 
-      this.searchToggle = document.querySelector(".nav-primary__search-toggle");
+    const searchToggle = document.querySelector(".nav-primary__search-toggle");
 
-      this.searchToggle.addEventListener("click", () => {
-        menuLeft.close();
-      });
+    searchToggle.addEventListener("click", () => {
+      menuLeft.close();
+    });
 
-      var slideMenuBackButtons = document.querySelectorAll(
-        ".slide-menu__backlink, .global-mobile-nav__audience-link"
-      );
+    var slideMenuBackButtons = document.querySelectorAll(
+      ".slide-menu__backlink, .global-mobile-nav__audience-link"
+    );
 
-      Array.prototype.forEach.call(slideMenuBackButtons, function (el, i) {
-        el.addEventListener("click", () => {
-          document.querySelector(".global-mobile-nav").scrollTop = 0;
-        });
-      });
-
-      // Responsive Resize Close menu and update toggles
-      window.addEventListener("resize", (event) => {
-        // Target Resize of LG ($screen-lg, 64rem, 1024px).
-        if (window.innerWidth > 1024) {
-          menuLeft.close(true);
-          //reset the menu toggle after closing.
-          this.mainNavToggle = document.querySelector(".nav-primary__toggle");
-          this.mainNavToggle.classList.remove(
-            "nav-primary__menu-toggle--is-open"
-          );
-          this.body = document.querySelector("body");
-          this.body.classList.remove("no-scroll");
-        }
+    Array.prototype.forEach.call(slideMenuBackButtons, function (el, i) {
+      el.addEventListener("click", () => {
+        document.querySelector(".global-mobile-nav").scrollTop = 0;
       });
     });
+
+    // Responsive Resize Close menu and update toggles
+    window.addEventListener("resize", (event) => {
+      // Target Resize of LG ($screen-lg, 64rem, 1024px).
+      if (window.innerWidth > 1024) {
+        menuLeft.close(true);
+        //reset the menu toggle after closing.
+        const mainNavToggle = document.querySelector(".nav-primary__toggle");
+        mainNavToggle.classList.remove(
+          "nav-primary__menu-toggle--is-open"
+        );
+        const body = document.querySelector("body");
+        body.classList.remove("no-scroll");
+      }
+    });
+
   },
 
   name: "Header interim",
@@ -85,45 +86,43 @@ export const Header = {
     const headerElem = canvasElement.querySelector(".uq-header");
     if (headerElem) new header(headerElem);
 
-    document.addEventListener("DOMContentLoaded", function () {
-      const menuLeftElem = document.getElementById("global-mobile-nav");
+    const menuLeftElem = document.getElementById("global-mobile-nav");
 
-      const menuLeft = new SlideMenu(menuLeftElem, {
-        position: "left",
-        submenuLinkAfter: " ",
-        backLinkBefore: " ",
+    const menuLeft = new SlideMenu(menuLeftElem, {
+      position: "left",
+      submenuLinkAfter: " ",
+      backLinkBefore: " ",
+    });
+
+    const searchToggle = document.querySelector(".nav-primary__search-toggle");
+
+    searchToggle.addEventListener("click", () => {
+      menuLeft.close();
+    });
+
+    var slideMenuBackButtons = document.querySelectorAll(
+      ".slide-menu__backlink, .global-mobile-nav__audience-link"
+    );
+
+    Array.prototype.forEach.call(slideMenuBackButtons, function (el, i) {
+      el.addEventListener("click", () => {
+        document.querySelector(".global-mobile-nav").scrollTop = 0;
       });
+    });
 
-      this.searchToggle = document.querySelector(".nav-primary__search-toggle");
-
-      this.searchToggle.addEventListener("click", () => {
-        menuLeft.close();
-      });
-
-      var slideMenuBackButtons = document.querySelectorAll(
-        ".slide-menu__backlink, .global-mobile-nav__audience-link"
-      );
-
-      Array.prototype.forEach.call(slideMenuBackButtons, function (el, i) {
-        el.addEventListener("click", () => {
-          document.querySelector(".global-mobile-nav").scrollTop = 0;
-        });
-      });
-
-      // Responsive Resize Close menu and update toggles
-      window.addEventListener("resize", (event) => {
-        // Target Resize of LG ($screen-lg, 64rem, 1024px).
-        if (window.innerWidth > 1024) {
-          menuLeft.close(true);
-          //reset the menu toggle after closing.
-          this.mainNavToggle = document.querySelector(".nav-primary__toggle");
-          this.mainNavToggle.classList.remove(
-            "nav-primary__menu-toggle--is-open"
-          );
-          this.body = document.querySelector("body");
-          this.body.classList.remove("no-scroll");
-        }
-      });
+    // Responsive Resize Close menu and update toggles
+    window.addEventListener("resize", (event) => {
+      // Target Resize of LG ($screen-lg, 64rem, 1024px).
+      if (window.innerWidth > 1024) {
+        menuLeft.close(true);
+        //reset the menu toggle after closing.
+        const mainNavToggle = document.querySelector(".nav-primary__toggle");
+        mainNavToggle.classList.remove(
+          "nav-primary__menu-toggle--is-open"
+        );
+        const body = document.querySelector("body");
+        body.classList.remove("no-scroll");
+      }
     });
   },
 

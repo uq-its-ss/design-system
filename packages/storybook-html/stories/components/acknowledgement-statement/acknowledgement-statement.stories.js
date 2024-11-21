4979f069583dfe67f8ecc0a96cae23bf0cdd3c3c
uqbcrunk <b.crunkhorn@uq.edu.au>
2024-11-21 11:49:10 +1000

    updated gradient

--- packages/storybook-html/stories/components/acknowledgement-statement/acknowledgement-statement.stories.js
+++ packages/storybook-html/stories/components/acknowledgement-statement/acknowledgement-statement.stories.js
@@ -10,21 +10,6 @@ export default {
   argTypes: {},
 };
 
-export const reconciliationStatement = {
-  args: {},
-  render: () => {
-    return `
-    <div  class="uq-acknowledgement-statement uq-acknowledgement-statement--large">
-      <div class="uq-acknowledgement-statement__content">
-        <div class="uq-acknowledgement-statement__text">
-          UQ acknowledges the Traditional Owners and their custodianship of the lands on which UQ is situated. &mdash; <a href="https://about.uq.edu.au/reconciliation" class="uq-acknowledgement-statement__link">Reconciliation at UQ</a>
-        </div>
-      </div>
-    </div>
-    `;
-  },
-};
-
 export const culturalAdvise = {
   args: {},
   render: () => {
@@ -33,6 +18,21 @@ export const culturalAdvise = {
       <div class="uq-acknowledgement-statement__content">
         <div class="uq-acknowledgement-statement__text">
           The Library is custodian of culturally sensitive <a href="https://web.library.uq.edu.au/collections/culturally-sensitive-collections" class="uq-acknowledgement-statement__link">Aboriginal and Torres Strait Islander materials</a>
+        </div>
+      </div>
+    </div>
+    `;
+  },
+};
+
+export const reconciliationStatement = {
+  args: {},
+  render: () => {
+    return `
+    <div  class="uq-acknowledgement-statement uq-acknowledgement-statement--large">
+      <div class="uq-acknowledgement-statement__content">
+        <div class="uq-acknowledgement-statement__text">
+          UQ acknowledges the Traditional Owners and their custodianship of the lands on which UQ is situated. &mdash; <a href="https://about.uq.edu.au/reconciliation" class="uq-acknowledgement-statement__link">Reconciliation at UQ</a>
         </div>
       </div>
     </div>

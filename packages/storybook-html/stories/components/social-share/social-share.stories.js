5604c64205400826d0a3d1c3f6c92be51045c60b
jptaranto <jacktaranto@gmail.com>
2025-01-17 11:57:30 +1100

    fix: update linkedin share link

--- packages/storybook-html/stories/components/social-share/social-share.stories.js
+++ packages/storybook-html/stories/components/social-share/social-share.stories.js
@@ -24,7 +24,7 @@ export default {
       </a>
     </li>
     <li class="uq-social-share__list__item">
-      <a href="http://www.linkedin.com/shareArticle?mini=true&url=${url}&aria-label=${title}" target="_blank" title="Share by Linkedin">
+      <a href="https://www.linkedin.com/feed/?shareActive=true&shareUrl=${url}&text=${title}" target="_blank" title="Share by Linkedin">
         <span class="uq-icon uq-icon--social--linkedin"></span>
       </a>
     </li>

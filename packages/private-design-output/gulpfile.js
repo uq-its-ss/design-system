fee235fc9e75a0e59a821111b2ec2c6077577d1b
Brett <github@crunkhorn.com.au>
2025-05-23 11:08:46 +1000

    chore: updated component references (#610)
    * chore: updated component references
    
    * chore: linting
    
    * chore: update package-lock
    
    * updated packages
    
    * fix: fix for storybook
    
    * linting
    
    * updates to static build

--- packages/private-design-output/gulpfile.js
+++ packages/private-design-output/gulpfile.js
@@ -1,12 +1,14 @@
 const { src, dest, parallel } = require("gulp"); // eslint-disable-line
 
 function exportImages() {
-  return src(["./src/images/*.*"]).pipe(dest("./dist/images"));
+  return src(["./src/images/**/*.*"], { encoding: false }).pipe(
+    dest("./dist/images"),
+  );
 }
 
 // Export favicon
 function exportFavicon() {
-  return src(["./src/favicon.ico"]).pipe(dest("./dist/"));
+  return src(["./src/favicon.ico"], { encoding: false }).pipe(dest("./dist/"));
 }
 
 function exportHtml() {

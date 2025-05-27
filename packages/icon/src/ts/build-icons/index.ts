b8cba9863342ff79471f87bd564a9b0c35f5e726
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-05-27 15:50:02 +1000

    fix: Format after generating icon code

--- packages/icon/src/ts/build-icons/index.ts
+++ packages/icon/src/ts/build-icons/index.ts
@@ -15,11 +15,11 @@ import { svgoOptions } from "./svgoOptions";
  */
 const optimiseSVG = (svgInput: string): string => {
     const result = optimize(svgInput, svgoOptions);
+    //@ts-ignore
+    if (result.data) {
         //@ts-ignore
-        if (result.data) {
-            //@ts-ignore
-            return result.data;
-        }
+        return result.data;
+    }
     throw new Error("Failed to optimise SVG using SVGO dependency.");
 };
 

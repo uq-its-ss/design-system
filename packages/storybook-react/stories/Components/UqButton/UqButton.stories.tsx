218bb1c34b42f35a7e4df73840018df54111a2b3
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-05-30 16:45:19 +1000

    feat: Proposal for new React button

---
+++ packages/storybook-react/stories/Components/UqButton/UqButton.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta } from "@storybook/react";
+import iconCatalog from "@uqds/icon";
+import { UqButton as Component } from "@uqds/react/tsx";
+
+export default {
+  component: Component,
+  argTypes: {
+    icon: {
+      options: [
+        '',
+        ...iconCatalog.map((icon) => `${icon.category}--${icon.name}`),
+      ],
+      control: "radio",
+    },
+  },
+  args: {
+    label: "Button",
+    expand: false,
+    spinner: false,
+    disabled: false,
+    onClick: () => alert('Button clicked'),
+  },
+} satisfies Meta<typeof Component>;
+
+export const base = {};

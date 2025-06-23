b057d7b7ce78e5be31a1722335c72850f162c495
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-06-23 14:41:21 +1000

    fix: Use SASS modules to resolve deprecation warnings

--- packages/icon/src/ts/build-icons/svgSassString.ts
+++ packages/icon/src/ts/build-icons/svgSassString.ts
@@ -3,34 +3,38 @@ export const colourPlaceholder = "~~COLOR~~";
 
 // Define Sass functions to use the icons list
 export const svgSassString = `
+    @use "sass:map";
+    @use "sass:string";
+    @use "sass:meta";
+
     @function -str-replace($string, $search, $replace: "") {
-      $index: str-index($string, $search);
-    
+      $index: string.index($string, $search);
+
       @if $index {
-        @return str-slice($string, 1, $index - 1) + $replace +
+        @return string.slice($string, 1, $index - 1) + $replace +
           -str-replace(
-            str-slice($string, $index + str-length($search)),
+            string.slice($string, $index + string.length($search)),
             $search,
             $replace
           );
       }
-    
+
       @return $string;
     }
-    
+
     @function get-icon($icon, $color: #000) {
-      @if type-of($color) != "color" and $color != "none" {
+      @if meta.type-of($color) != "color" and $color != "none" {
         @warn 'The requested color - "' + $color + '" - was not recognized as a Sass color value.';
         @return null;
       }
-    
-      @if map-has-key($-icons, $icon) {
-        $icon: map-get($-icons, $icon);
+
+      @if map.has-key($-icons, $icon) {
+        $icon: map.get($-icons, $icon);
         $data-uri: -str-replace($icon, "${colourPlaceholder}", $color);
-    
-        @return -str-replace($data-uri, "#", "%23");
+
+        @return $data-uri;
       }
-    
+
       @warn 'The requested icon - "' + $icon + '" - is not defined in the $icons map.';
       @return null;
     }\n`;

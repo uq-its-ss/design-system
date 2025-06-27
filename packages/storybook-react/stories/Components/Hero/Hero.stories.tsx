a1c8bc1dd9ebd5967ed1e1a1f3393b72d9eff5be
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-06-27 12:34:17 +1000

    feat: Allow React Hero component to specify the link component
    Defaults to HTML `a` element but can be changed to React Router `Link`
    component if desired.

--- packages/storybook-react/stories/Components/Hero/Hero.stories.tsx
+++ packages/storybook-react/stories/Components/Hero/Hero.stories.tsx
@@ -6,10 +6,15 @@ export default {
   args: {
     eyebrow: "Step 1 of 8",
     title: "Choose your program",
-    back: "Postgraduate coursework",
-    backOnClick: () => {
-      alert("Back clicked.");
-    }
+    backText: "Postgraduate coursework",
+    backComponent: "a",
+    backComponentProps: {
+      href: "#",
+      onClick: (ev: React.MouseEvent<HTMLAnchorElement>) => {
+        alert("Back link clicked.");
+        ev.preventDefault();
+      },
+    },
   },
 } satisfies Meta<typeof Component>;
 

d7905ceaf4859ce70b6515c3bcacefe4256f6dd7
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-06-27 23:29:30 +1000

    feat: Add React component for Table

---
+++ packages/react/src/jsx/components/Table/Table.tsx
@@ -0,0 +1,18 @@
+import type { FC, ReactNode } from "react";
+import css from "./styles.module.scss";
+
+export interface TableProps {
+  /** Normal table content - thead, tr, td, td, etc. */
+  children: ReactNode;
+};
+
+/** A normal HTML table with UQDS styles applied. */
+export const Table: FC<TableProps> = ({
+  children,
+}) => (
+  <div className={css["uq-table__wrapper"]}>
+    <table className={css["uq-table"]}>
+      {children}
+    </table>
+  </div>
+);

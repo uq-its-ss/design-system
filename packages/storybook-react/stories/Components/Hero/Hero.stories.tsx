691ebfcf10331b44deb8f437dcb8af93dd591d57
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-06-27 13:37:07 +1000

    feat: Add Hero showcase to React storybook
    This provides an example of how to use a custom back link component.

--- packages/storybook-react/stories/Components/Hero/Hero.stories.tsx
+++ packages/storybook-react/stories/Components/Hero/Hero.stories.tsx
@@ -1,3 +1,4 @@
+import React, { type FC } from "react";
 import { Meta } from "@storybook/react";
 import { Hero as Component } from "@uqds/react/tsx";
 
@@ -17,5 +18,57 @@ export default {
     },
   },
 } satisfies Meta<typeof Component>;
+
+// Dummy Link component to demonstrate using different back components.
+interface LinkProps {
+  children: React.ReactNode;
+};
+const Link: FC<LinkProps> = ({ children }) => (
+  <a href="#" onClick={(ev) => {
+    alert("Custom component clicked.");
+    ev.preventDefault();
+  }}>
+    {children}
+  </a>
+);
+
+export const showcase = {
+  render: () => (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      gap: "0px"
+    }}>
+      <Component
+        eyebrow={undefined}
+        title="Title Only"
+        backText={undefined}
+      />
+      <Component
+        eyebrow="With Eyebrow"
+        title="Title"
+        backText={undefined}
+      />
+      <Component
+        eyebrow={undefined}
+        title="Title"
+        backText="Backwards navigation"
+        backComponentProps={{
+          href: "#",
+          onClick: (ev: React.MouseEvent<HTMLAnchorElement>) => {
+            alert("Back link clicked.");
+            ev.preventDefault();
+          }
+        }}
+      />
+      <Component
+        eyebrow="Custom back link component"
+        title="Title"
+        backText="Link text"
+        backComponent={Link}
+      />
+    </div>
+  ),
+};
 
 export const base = {};

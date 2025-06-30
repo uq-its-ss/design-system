541a54f4ce9bc13cce2c3441dd0f14554de889a3
Adam Nielsen <adam.nielsen@uq.edu.au>
2025-06-30 13:41:55 +1000

    feat: React/LoadingSpinner - Add 'visible' property to show/hide element

--- packages/react/src/jsx/components/LoadingSpinner/LoadingSpinner.tsx
+++ packages/react/src/jsx/components/LoadingSpinner/LoadingSpinner.tsx
@@ -8,16 +8,22 @@ export interface LoadingSpinnerProps {
 
   /** Size of animation. */
   size?: "small" | "default";
+
+  /** Show/hide the alert.  If omitted, always visible.  This is to allow it
+   * to be dynamically shown without causing a virtual DOM update. */
+  visible?: boolean;
 }
 
 /** A button the user can click on to perform an action. */
 export const LoadingSpinner: FC<LoadingSpinnerProps> = ({
   variant = "dark",
   size = "default",
+  visible = true,
 }) => {
   return (
     <div
       className={classNames({
+        [css["visually-hidden"]]: visible === false,
         [css["uq-loading-spinner"]]: true,
         [css["uq-loading-spinner--light"]]: variant === "light",
         [css["uq-loading-spinner--small"]]: size === "small",

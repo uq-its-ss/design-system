ff5c8e56d03adeac25eb1687ed39e994cc5bff7c
uqbcrunk <b.crunkhorn@uq.edu.au>
2025-08-04 15:37:01 +1000

    updates

---
+++ packages/storybook-email/stories/templates/oracle/oracle.stories.js
@@ -0,0 +1,402 @@
+export default {
+  title: "Templates/Oracle",
+  parameters: {
+    story: {
+      inline: false,
+    },
+  },
+};
+
+export const oracle = {
+  render: () => `
+
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:rn="http://schemas.rightnow.com/crm/document" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" lang="en" xml:lang="en">
+<head>
+<link href="/euf/core/static/ma.css" rel="stylesheet" type="text/css"/>
+<meta name="robots" content="noindex, nofollow" />
+<meta name="referrer" content="no-referrer" /><!--[if gte mso 9]>
+            <xml>
+                <o:OfficeDocumentSettings> <o:AllowPNG /> <o:PixelsPerInch>96</o:PixelsPerInch> </o:OfficeDocumentSettings>
+            </xml>
+        <![endif]-->
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="format-detection" content="date=no" />
+<meta name="format-detection" content="address=no" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="color-scheme" content="light dark" />
+<meta name="supported-color-schemes" content="light dark" />
+<title>The University of Queensland</title>
+<!--[if gte mso 9]>
+            <style type="text/css" media="all">
+                sup {
+                    font-size: 100% !important;
+                }
+            </style>
+        <![endif]-->
+<!-- NON-OUTLOOK @import FONT CSS GOES HERE - BUGGY IN ORACLE -->
+<link href="https://static.uq.net.au/v1/fonts/Roboto/roboto.css" rel="stylesheet" type="text/css" media="all" />
+<style type="text/css">
+            /*<![CDATA[*/
+            body {
+                padding: 0 !important;
+                margin: 0 auto !important;
+                display: block !important;
+                min-width: 100% !important;
+                width: 100% !important;
+                background: #c7c7c7;
+                -webkit-text-size-adjust: none;
+            }
+            a {
+                color: #51247a;
+                text-decoration: underline;
+            }
+            p {
+                margin-left: 0 !important;
+                margin-right: 0 !important;
+            }
+            img {
+                margin: 0 !important;
+                -ms-interpolation-mode: bicubic; /* Allow smoother rendering of resized image in Internet Explorer */
+            }
+            strong {
+                font-weight: 700 !important;
+            }
+            .main-table div {
+                background: transparent !important;
+            }
+            #MessageViewBody,
+            #MessageWebViewDiv {
+                width: 100% !important;
+            } /* Samsung Galaxy Note 4 Samsung Mail - make email stay centered */
+
+            a.skip-main {
+                left: -999px;
+                position: absolute;
+                top: auto;
+                width: 1px;
+                height: 1px;
+                overflow: hidden;
+                z-index: -999;
+            }
+            a.skip-main:focus,
+            a.skip-main:active {
+                color: #ffffff;
+                background-color: #ffffff;
+                left: auto;
+                top: auto;
+                width: 30%;
+                height: auto;
+                overflow: auto;
+                margin: 10px 35%;
+                padding: 5px;
+                border-radius: 15px;
+                border: 4px solid yellow;
+                text-align: center;
+                font-size: 12em;
+                z-index: 999;
+            }
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+                padding: 0 !important;
+                margin: 0 !important;
+                mso-line-height-rule: exactly;
+                margin: 0;
+            }
+
+            .l-white a {
+                color: #ffffff;
+            }
+
+            :root {
+                color-scheme: light dark;
+                supported-color-schemes: light dark;
+            }
+
+            @media (prefers-color-scheme: dark) {
+                .title {
+                    color: #ffffff !important;
+                }
+                .text {
+                    color: #e8e8e8 !important;
+                }
+                a {
+                    color: #c29fdf !important;
+                }
+
+                .c-white,
+                .l-white a {
+                    color: #ffffff !important;
+                }
+
+                .bg-purple {
+                    background-color: #51247a !important;
+                }
+                .bg-grey-f7 {
+                    background-color: #302c33 !important;
+                }
+                .bg-dgrey {
+                    background-color: #19151c !important;
+                }
+
+            }
+
+            [data-ogsc] .title {
+                color: #ffffff !important;
+            }
+            [data-ogsc] .text {
+                color: #e8e8e8 !important;
+            }
+            [data-ogsc] a {
+                color: #c29fdf !important;
+            }
+            [data-ogsc] .c-white,
+            [data-ogsc] .l-white a {
+                color: #ffffff !important;
+            }
+            [data-ogsc] .bg-purple {
+                background-color: #51247a !important;
+            }
+            [data-ogsc] .bg-grey-f7 {
+                background-color: #302c33 !important;
+            }
+            [data-ogsc] .bg-dgrey {
+                background-color: #19151c !important;
+            }
+
+            /* Mobile styles */
+            @media only screen and (max-device-width: 480px), only screen and (max-width: 480px) {
+ 
+                body,
+                .body {
+                    background-color: #fffffe !important;
+                }
+
+                .fluid-img img {
+                    width: 100% !important;
+                    max-width: 100% !important;
+                    height: auto !important;
+                    max-height: auto !important;
+                }
+
+                .m-hide {
+                    display: none !important;
+                    width: 0 !important;
+                    height: 0 !important;
+                    font-size: 0 !important;
+                    line-height: 0 !important;
+                    min-height: 0 !important;
+                }
+
+                .mw-100p {
+                    width: 100% !important;
+                }
+            }
+
+            /*]]>*/
+</style>
+</head>
+<body class="body bg-grey-c7" style="padding: 0 !important; margin: 0 !important; display: block !important; min-width: 100% !important; width: 100% !important; background: #c7c7c7; -webkit-text-size-adjust: none; background-color: #c7c7c7;">
+
+
+<center>
+    <table class="main-table bg-grey-c7" width="100%" border="0" cellspacing="0" cellpadding="0" style="margin: 0; padding: 0; width: 100%; height: 100%; background-color:#C7C7C7;" bgcolor="#C7C7C7">
+      <tr>
+        <td style="margin: 0; padding: 0; width: 100%; height: 100%;" align="center" valign="top">
+          <table class="m-hide" style="mso-hide: all;" width="100%" border="0" cellspacing="0" cellpadding="0"><!-- Preview Text -->
+            <tr>
+              <td style="font-size: 0pt; line-height: 0pt;"></td>
+            </tr>
+            <!-- END Preview Text --><!-- Fix -->
+            <tr>
+       <td style="font-size: 0pt; line-height: 0pt;">
+                                                                                                                                                                                                                                                                  
+        <a class="skip-main" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;" href="#main" aria-hidden="true">Skip to main content</a>
+       </td>
+      </tr>
+          <!-- END Fix --></table>
+          <table class="mw-100p bg-dgrey" width="640" border="0" cellspacing="0" cellpadding="0" style="background-color:#ffffff;">
+            <tr>
+              <td style="font-size: 0pt; line-height: 0pt;"><!---->
+                <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="min-width: 100%; ">
+                  <tr>
+                    <td><!-- Header -->
+                      <table border="0" cellpadding="0" cellspacing="0" class="bg-purple" style="background-color:#51247A;" width="100%">
+                        <tr>
+                          <td style="padding-top: 32px; padding-left: 24px; padding-right: 24px; padding-bottom: 32px;">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                              <tr>
+                               <td class="img" style="font-size:0pt; line-height:0pt; text-align:left;" width="304"><a target="_blank" href="https://support.my.uq.edu.au/rd?1=AvMd~wq5Dv8S~xb~Gv8h~yLqu70pBS77Mv_r~z7~Pv9i&amp;2=85827"><img alt="The University of Queensland logo" height="42" src="https://content.app.uq.edu.au/axugymxgvs/oracle-template/uq-orac-logo.png" style="padding: 0px; text-align: center;" /></a></td>
+                                <td class="text c-white l-white" style="font-family:'Roboto', Arial, sans-serif; font-size: 16px; line-height: 20px; text-align:right; color:#fffffe; padding-bottom: 10px;padding-left:20px;" width="316">View in browser</td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                    <!-- END Header --></td>
+                  </tr>
+                </table>
+<!---->
+<!---->
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="width:100%!important;min-width: 100%!important">
+<tr>
+<td>
+<table border="0" cellpadding="0" cellspacing="0" class="bg-purple" style="background-color:#51247A;width:100%!important" width="100%">
+<tr>
+<td style="padding-top: 16px; padding-bottom: 16px; padding-left: 24px; padding-right: 24px;">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%!important" width="100%">
+<tr>
+<td class="title c-white l-white" style="font-family:'Roboto', Arial, sans-serif; font-size: 20px; line-height: 24px; text-align:left; color:#ffffff;font-weight:bold;width:100%!important"><!-- Purple Intro Banner Primary Heading -->
+Academic Services
+<!-- END Purple Intro Banner Primary Heading --></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<!---->
+
+<!---->
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="background-color: transparent; min-width: 100%; ">
+<tr>
+<td style="padding: 24px 24px 40px 24px; ">
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+<td>
+<table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"><!-- Body Text -->
+<tr>
+<td class="text" style="color:#3b383e; font-family:'Roboto', Arial, sans-serif; font-size: 16px; line-height: 26px; text-align: left;">
+<!-- Body Text -->
+
+<p style="FONT-SIZE: 24px; MARGIN-BOTTOM: 8px; FONT-WEIGHT: bold; LINE-HEIGHT: 29px">Email headline</p>
+<p style="MARGIN-TOP: 0px">Email description (optional)</p>
+<p>Dear Mark,</p>
+<!-- Email Copy -->
+<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velsit essssse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+<p style="FONT-SIZE: 24px; MARGIN-BOTTOM: 8px; FONT-WEIGHT: bold; LINE-HEIGHT: 29px">Heading - 24px</p>
+<p style="FONT-SIZE: 18px; MARGIN-TOP: 0px; FONT-WEIGHT: bold; LINE-HEIGHT: 22px">Sub heading - 18px</p>
+<!-- Purple Button -->
+<table role="presentation" cellspacing="0" cellpadding="0" width="100%" border="0">
+<tbody>
+<tr>
+<td>
+<table style="BACKGROUND-COLOR: #51247a; border-radius: 4px" cellspacing="0" cellpadding="0" border="0">
+<tbody>
+<tr>
+<td class="btn btnp-9-20 fz-14 lh-18 c-white l-white" style="FONT-SIZE: 16px; FONT-FAMILY: 'Roboto', Arial, sans-serif; COLOR: #ffffff; TEXT-ALIGN: center; LINE-HEIGHT: 18px; mso-padding-alt: 16px 24px">
+<p><a title="Primary button" class="link" style="TEXT-DECORATION: none; COLOR: #fffffe; PADDING-BOTTOM: 4px; PADDING-TOP: 4px; PADDING-LEFT: 24px; DISPLAY: block; PADDING-RIGHT: 24px" href="ADD BUTTON URL HERE" target="_blank">Primary button</a></p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<!-- END Button --><!-- Contact Details -->
+<p>Kind regards<br />
+First name Last name<br />
+Job Title<br />
+The University of Queensland</p>
+<p><strong>Contact</strong><br />
+<a style="TEXT-DECORATION: underline" href="mailto:ADD EMAIL ADDRESS HERE">email@uq.edu.au</a><br />
++61 7 0000 0000</p>
+
+<!-- END Body Text -->
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<!---->
+<!---->
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="width:100%!important;min-width:100%!important">
+<tr>
+<td>
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="width:100%!important">
+<tr><!-- Purple block -->
+<td style="font-size:1pt; line-height:1pt;height: 80px;background-color:#51247a;width:100%!important"> </td>
+<!-- END Purple block --></tr>
+</table>
+</td>
+</tr>
+</table>
+<!---->
+<!---->
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="min-width: 100%; ">
+<tr>
+<td><!-- Divider - 24px -->
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+<td style="font-size:1px;line-height:24px" width="100%"></td>
+</tr>
+</table>
+<!-- END Divider - 24px --></td>
+</tr>
+</table>
+<!---->
+
+
+                <!---->
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="min-width: 100%; ">
+<tr>
+<td><!-- Footer -->
+<table border="0" cellpadding="0" cellspacing="0" class="bg-dgrey" style="background-color:#ffffff;" width="100%">
+<tr>
+<td style="padding-top: 24px; padding-bottom: 20px; padding-left: 25px; padding-right: 25px;">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+<tr>
+<td class="text" style="color:#3B383E; font-family:'Roboto', Arial, sans-serif; font-size: 18px; line-height: 29px; text-align:left; padding-bottom: 20px;">
+UQ acknowledges the Traditional Owners and their custodianship of the lands on which UQ is situated. <a style="color:#51247a;text-decoration:underline;font-weight:normal" target="_blank" href="https://support.my.uq.edu.au/rd?1=AvMd~wq5Dv8S~xb~Gv8h~yLqu70pBS77Mv_r~z7~Pv9i&amp;2=85830">Reconciliation at UQ</a>
+</td>
+</tr>
+<tr>
+<td class="text" style="color:#3B383E; font-family:'Roboto', Arial, sans-serif; font-size: 16px; line-height: 26px; text-align:left; padding-bottom: 20px;">
+The University of Queensland, St Lucia Qld 4072 Australia<br />
+<a style="color:#51247a;text-decoration:underline;font-weight:normal" target="_blank" href="https://support.my.uq.edu.au/rd?1=AvMd~wq5Dv8S~xb~Gv8h~yLqu70pBS77Mv_r~z7~Pv9i&amp;2=85831">CRICOS 00025B</a> | <a style="color:#51247a;text-decoration:underline;font-weight:normal" target="_blank" href="https://support.my.uq.edu.au/rd?1=AvMd~wq5Dv8S~xb~Gv8h~yLqu70pBS77Mv_r~z7~Pv9i&amp;2=85832">TEQSA PRV12080</a>
+</td>
+</tr>
+<tr>
+<td class="text" style="color:#3B383E; font-family:'Roboto', Arial, sans-serif; font-size: 16px; line-height: 26px; text-align:left; padding-bottom: 10px;">
+Read <a style="color:#51247a;text-decoration:underline;font-weight:normal" href="https://support.my.uq.edu.au/rd?1=AvMd~wq5Dv8S~xb~Gv8h~yLqu70pBS77Mv_r~z7~Pv9i&amp;2=89403">UQ's Privacy policy</a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<!-- END Footer --></td>
+</tr>
+</table>
+
+
+<!---->
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
+
+
+`,
+};

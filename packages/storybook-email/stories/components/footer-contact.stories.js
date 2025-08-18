e807eb849287618e3d6e36f2a8273da4d84d8f04
uqbcrunk <b.crunkhorn@uq.edu.au>
2025-08-18 15:46:29 +1000

    updates

--- packages/storybook-email/stories/components/footer-contact.stories.js
+++ packages/storybook-email/stories/components/footer-contact.stories.js
@@ -312,3 +312,29 @@ export const footerContactInternational = {
 
 `,
 };
+
+export const contactArea = {
+  render: () => `
+
+<!-- Contact Banner -->
+<p><strong>Contact UQ Area Name</strong><br>
+Monday to Friday 8:30am-5pm AEST<br>
+<a style="color:#51247a; text-decoration:underline;" href="mailto:ADD EMAIL ADDRESS HERE">email@uq.edu.au</a><br>
++61 7 0000 0000<br>
+<a style="color:#51247a; text-decoration:underline;" href="LINK HERE">Visit UQ Area Name website</a></p>
+<!-- END Contact Banner -->
+
+`,
+};
+
+export const contact = {
+  render: () => `
+
+<!-- Contact Banner -->
+<p><strong>Contact</strong><br>
+<a style="color:#51247a; text-decoration:underline;" href="mailto:ADD EMAIL ADDRESS HERE">email@uq.edu.au</a><br>
++61 7 0000 0000</p>
+<!-- END Contact Banner -->
+
+`,
+};

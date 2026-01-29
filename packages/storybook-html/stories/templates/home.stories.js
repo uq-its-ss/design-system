386c0ad36c2f168eb49f9191ce83427b625b6762
uqnm <n.millington@uq.edu.au>
2026-01-29 12:39:04 +1000

    ITSADSSD-65908: add id values to header and footer for anchor targeting (#721)
    * fix: add id values to header and footer for anchor targeting
    * fix: header stories id duplicate
    * fix: add focusable states for .visibility-hidden utility class
    * fix: add skip links to body doco
    * fix: update sbdocs to target correct classes. apply robot across doco
    * fix: sb docs and toc styling tweaks
    * fix: add code monospace override
    * fix: update skip links doco, customize source code output
    * fix: add main wrapper and skip links to page templates. TODO: clean up skip link templating
    * fix: linting and code cleanup
    * fix: minor get started cleanup
    * fix: home template structure

--- packages/storybook-html/stories/templates/home.stories.js
+++ packages/storybook-html/stories/templates/home.stories.js
@@ -33,94 +33,103 @@ export default {
 
 export const Home = {
   render: ({ showGrid }) => `
-<div class="${classNames({ "uqds-layout-demo-bg": showGrid })}">
+<div class="${classNames({ "uqds-layout-preview": showGrid })}">
+  <a href="#uq-header" class="visually-hidden focusable">Skip to header</a>
+    <a href="#uq-main" class="visually-hidden focusable">Skip to content</a>
+    <a href="#uq-footer" class="visually-hidden focusable">Skip to footer</a>
   ${Header()}
-  ${Hero()}
-  ${Section({
-    title: "",
-    description: "",
-    content: `
-<div class="uq-section">
-<div class="uq-card-grid uq-card-grid--target-4x">
-  ${CardIconNav({
-    title: "Study",
-    description:
-      "Find study options to match your interests and learn how to get the most out of your time at UQ.",
-    icon: "arts-humanities-and-social-sciences--book-open-bookmark",
-  })}
-  ${CardIconNav({
-    title: "Research",
-    description:
-      "Our research is creating change and making an impact on the world’s biggest challenges.",
-    icon: "science-and-mathematics--science-molecule",
-  })}
-  ${CardIconNav({
-    title: "Partners and community",
-    description: "We collaborate locally and globally to drive innovation.",
-    icon: "other--tool-box",
-  })}
-  ${CardIconNav({
-    title: "About UQ",
-    description:
-      "We educate and work with outstanding people to build a better future.",
-    icon: "architecture-and-planning--school-building",
-  })}
-</div>
-</div>
-      `,
-  })}
+  <main id="uq-main">
+    ${Hero()}
     ${Section({
       title: "",
       description: "",
       content: `
-<div class="uq-section">
-<div class="uq-card-grid uq-card-grid--target-3x">
-  ${CardImage({
-    title: "Our campuses",
-    description:
-      "Our campuses boast world-class facilities, beautiful outdoor areas, and a range of sporting and cultural amenities.",
-    image: "images/card/card-example-herston-campus.jpg",
-  })}
-  ${CardImage({
-    title: "Apply now",
-    description: "View the steps to apply and get started on your application.",
-    image: "images/card/card-example-st-lucia-campus.jpg",
-  })}
-  ${CardImage({
-    title: "Scholarships",
-    description:
-      "Apply for financial support to enrich your time at UQ and help you to focus on your studies.",
-    image: "images/card/card-example-gatton-campus.jpg",
-  })}
-</div>
-</div>
+        <div class="uq-section">
+          <div class="uq-card-grid uq-card-grid--target-4x">
+            ${CardIconNav({
+              title: "Study",
+              description:
+                "Find study options to match your interests and learn how to get the most out of your time at UQ.",
+              icon: "arts-humanities-and-social-sciences--book-open-bookmark",
+            })}
+            ${CardIconNav({
+              title: "Research",
+              description:
+                "Our research is creating change and making an impact on the world’s biggest challenges.",
+              icon: "science-and-mathematics--science-molecule",
+            })}
+            ${CardIconNav({
+              title: "Partners and community",
+              description:
+                "We collaborate locally and globally to drive innovation.",
+              icon: "other--tool-box",
+            })}
+            ${CardIconNav({
+              title: "About UQ",
+              description:
+                "We educate and work with outstanding people to build a better future.",
+              icon: "architecture-and-planning--school-building",
+            })}
+          </div>
+        </div>
       `,
     })}
     ${Section({
-      title: "News",
-      description: `<a href="#">See more news</a>`,
+      title: "",
+      description: "",
       content: `
-<div class="uq-section">
-<div class="uq-card-grid uq-card-grid--target-3x">
-  ${NewsCard({
-    title: "Lifelike lab-grown skin developed from human stem cells",
-    description: "",
-    image: "images/card/card-example-gatton-campus.jpg",
-  })}
-  ${NewsCard({
-    title: "Zinc discovery holds promise for people with cystic fibrosis",
-    description: "",
-    image: "images/card/card-example-herston-campus.jpg",
-  })}
-  ${NewsCard({
-    title: "A lightweight fish pen to move farms to deeper seas",
-    description: "",
-    image: "images/card/card-example-st-lucia-campus.jpg",
-  })}
-</div>
-</div>
-      `,
+          <div class="uq-section">
+            <div class="uq-card-grid uq-card-grid--target-3x">
+              ${CardImage({
+                title: "Our campuses",
+                description:
+                  "Our campuses boast world-class facilities, beautiful outdoor areas, and a range of sporting and cultural amenities.",
+                image: "images/card/card-example-herston-campus.jpg",
+              })}
+              ${CardImage({
+                title: "Apply now",
+                description:
+                  "View the steps to apply and get started on your application.",
+                image: "images/card/card-example-st-lucia-campus.jpg",
+              })}
+              ${CardImage({
+                title: "Scholarships",
+                description:
+                  "Apply for financial support to enrich your time at UQ and help you to focus on your studies.",
+                image: "images/card/card-example-gatton-campus.jpg",
+              })}
+            </div>
+          </div>
+        `,
     })}
-  ${footer()}
-</div>`,
+      ${Section({
+        title: "News",
+        description: `<a href="#">See more news</a>`,
+        content: `
+          <div class="uq-section">
+            <div class="uq-card-grid uq-card-grid--target-3x">
+              ${NewsCard({
+                title:
+                  "Lifelike lab-grown skin developed from human stem cells",
+                description: "",
+                image: "images/card/card-example-gatton-campus.jpg",
+              })}
+              ${NewsCard({
+                title:
+                  "Zinc discovery holds promise for people with cystic fibrosis",
+                description: "",
+                image: "images/card/card-example-herston-campus.jpg",
+              })}
+              ${NewsCard({
+                title: "A lightweight fish pen to move farms to deeper seas",
+                description: "",
+                image: "images/card/card-example-st-lucia-campus.jpg",
+              })}
+            </div>
+          </div>    
+        `,
+      })}
+    </main>  
+    ${footer()}
+  </div>`,
 };

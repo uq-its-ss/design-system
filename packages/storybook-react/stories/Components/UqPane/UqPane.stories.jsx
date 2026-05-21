9aedf96d9d73908afd166f5207798e7a1c258313
Nate Millington <n.millington@uq.edu.au>
2026-05-22 09:51:11 +1000

    fix: storybook controls for uqcard, uqstorycard and uqpane

--- packages/storybook-react/stories/Components/UqPane/UqPane.stories.jsx
+++ packages/storybook-react/stories/Components/UqPane/UqPane.stories.jsx
@@ -1,5 +1,6 @@
 import { reactCardDecorator } from "../../../lib/decorators";
 import { UqPane as Component, UqPaneVariant, UqIcon } from "@uqds/react";
+import iconCatalog from "@uqds/icon";
 
 export default {
   title: "Components/UqPane",
@@ -10,23 +11,107 @@ export default {
       control: "select",
       options: Object.values(UqPaneVariant),
     },
+    title: {
+      control: "text",
+      description: "The title text for the pane",
+    },
+    descriptionText: {
+      control: "text",
+      description: "The description text for the pane",
+    },
+    imageSrc: {
+      control: "text",
+      description: "The image source URL",
+    },
+    imageAlt: {
+      control: "text",
+      description: "The image alt text",
+    },
+    iconName: {
+      control: "select",
+      options: [
+        "",
+        ...iconCatalog.map((icon) => `${icon.category}--${icon.name}`),
+      ],
+      description: "The icon to display (only for icon variants)",
+    },
+    linkText: {
+      control: "text",
+      description: "The link text",
+    },
+    linkHref: {
+      control: "text",
+      description: "The link URL",
+    },
+    videoUrl: {
+      control: "text",
+      description: "The YouTube video URL or embed URL",
+    },
+    // Hide ReactNode props since they can't be controlled directly
+    description: {
+      control: false,
+    },
+    image: {
+      control: false,
+    },
+    icon: {
+      control: false,
+    },
+    links: {
+      control: false,
+    },
+    video: {
+      control: false,
+    },
   },
   args: {
     variant: UqPaneVariant.image,
     title: "Settling in",
-    description:
+    descriptionText:
       "Settle in to uni life quicker with guides on everything from Orientation to campus safety.",
-    image: <img src="images/card/card-example-herston-campus.jpg" alt="" />,
-    icon: <UqIcon name="agriculture-and-environment--ecology-leaf" />,
-    links: <a href="#">This is a link</a>,
-    video: (
+    imageSrc: "images/card/card-example-herston-campus.jpg",
+    imageAlt: "",
+    iconName: "agriculture-and-environment--ecology-leaf",
+    linkText: "This is a link",
+    linkHref: "#",
+    videoUrl: "https://www.youtube.com/embed/ViMYZ8PRAjQ?si=MjVUL2on656-WESr",
+  },
+  render: ({
+    variant,
+    title,
+    descriptionText,
+    imageSrc,
+    imageAlt,
+    iconName,
+    linkText,
+    linkHref,
+    videoUrl,
+  }) => {
+    const description = descriptionText || undefined;
+    const image = imageSrc ? <img src={imageSrc} alt={imageAlt} /> : undefined;
+    const icon = iconName ? <UqIcon name={iconName} /> : undefined;
+    const links =
+      linkText && linkHref ? <a href={linkHref}>{linkText}</a> : undefined;
+    const video = videoUrl ? (
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/ViMYZ8PRAjQ?si=MjVUL2on656-WESr"
-        allowfullscreen
+        src={videoUrl}
+        allowFullScreen
       ></iframe>
-    ),
+    ) : undefined;
+
+    return (
+      <Component
+        variant={variant}
+        title={title}
+        description={description}
+        image={image}
+        icon={icon}
+        links={links}
+        video={video}
+      />
+    );
   },
 };
 
--- packages/storybook-react/stories/Components/UqStoryCard/UqStoryCard.stories.jsx
+++ packages/storybook-react/stories/Components/UqStoryCard/UqStoryCard.stories.jsx
@@ -10,15 +10,84 @@ export default {
       control: "select",
       options: Object.values(UqStoryCardVariant),
     },
+    titleText: {
+      control: "text",
+      description: "The title text for the story card",
+    },
+    titleHref: {
+      control: "text",
+      description: "The URL for the title link (leave empty for no link)",
+    },
+    descriptionText: {
+      control: "text",
+      description: "The description text for the story card",
+    },
+    imageSrc: {
+      control: "text",
+      description: "The image source URL",
+    },
+    imageAlt: {
+      control: "text",
+      description: "The image alt text",
+    },
+    topLabel: {
+      control: "text",
+      description: "The top label (e.g., 'News', 'Event')",
+    },
+    bottomLabel: {
+      control: "text",
+      description: "The bottom label (e.g., date)",
+    },
+    // Hide ReactNode props since they can't be controlled directly
+    title: {
+      control: false,
+    },
+    description: {
+      control: false,
+    },
+    image: {
+      control: false,
+    },
   },
   args: {
     variant: UqStoryCardVariant.feature,
-    title: <a href="#">How does ATAR work and what does it mean?</a>,
-    description:
+    titleText: "How does ATAR work and what does it mean?",
+    titleHref: "#",
+    descriptionText:
       "The University of Queensland is honouring a learning scientist who is leading by example to inspire and teach future teachers.",
-    image: <img src="images/card/card-example-herston-campus.jpg" alt="" />,
+    imageSrc: "images/card/card-example-herston-campus.jpg",
+    imageAlt: "",
     topLabel: "News",
     bottomLabel: "20 September 2023",
+  },
+  render: ({
+    variant,
+    titleText,
+    titleHref,
+    descriptionText,
+    imageSrc,
+    imageAlt,
+    topLabel,
+    bottomLabel,
+  }) => {
+    const title = titleHref ? (
+      <a href={titleHref}>{titleText}</a>
+    ) : (
+      titleText
+    );
+    const description = descriptionText || undefined;
+    const image = imageSrc ? <img src={imageSrc} alt={imageAlt} /> : undefined;
+
+    return (
+      <Component
+        variant={variant}
+        title={title}
+        description={description}
+        image={image}
+        topLabel={topLabel}
+        bottomLabel={bottomLabel}
+      />
+    );
   },
 };
 

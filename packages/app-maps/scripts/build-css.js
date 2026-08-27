cee187a591aa30064f618c5fe59c333e1c6ce9cf
Bcrunk <b.crunkhorn@uq.edu.au>
2026-08-27 14:18:25 +1000

    fix(app-maps): updated node ref

--- packages/app-maps/scripts/build-css.js
+++ packages/app-maps/scripts/build-css.js
@@ -4,25 +4,38 @@ var fs = require("fs"),
 sass.render(
   {
     file: "./src/scss/_component.scss",
-    includePaths: ["./node_modules", "./node_modules/@uqds/core/node_modules"],
+    includePaths: [
+      "./node_modules",
+      "../../node_modules", // Root node_modules for workspace dependencies
+      "./node_modules/@uqds/core/node_modules"
+    ],
   },
   (err, result) => {
-    if (!err) {
-      fs.mkdir("./dist/css/", { recursive: true }, function (err) {
-        // @todo eslint solution needs to be reviewed
-        // eslint-disable-next-line no-console
-        console.error(err);
-      });
-
-      fs.writeFile("./dist/css/app-maps.css", result.css, function (err) {
-        // @todo eslint solution needs to be reviewed
-        // eslint-disable-next-line no-console
-        console.error(err);
-      });
-    } else {
+    if (err) {
       // @todo eslint solution needs to be reviewed
       // eslint-disable-next-line no-console
       console.error(err);
+      process.exit(1);
     }
+
+    fs.mkdir("./dist/css/", { recursive: true }, function (mkdirErr) {
+      if (mkdirErr) {
+        // @todo eslint solution needs to be reviewed
+        // eslint-disable-next-line no-console
+        console.error("Error creating dist/css directory:", mkdirErr);
+        process.exit(1);
+      }
+
+      fs.writeFile("./dist/css/app-maps.css", result.css, function (writeErr) {
+        if (writeErr) {
+          // @todo eslint solution needs to be reviewed
+          // eslint-disable-next-line no-console
+          console.error("Error writing CSS file:", writeErr);
+          process.exit(1);
+        }
+        // eslint-disable-next-line no-console
+        console.log("✓ CSS built successfully");
+      });
+    });
   },
 );

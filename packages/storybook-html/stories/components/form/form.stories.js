7f876f228b35aed481a832b88a41617ee543a7cd
Nate Millington <n.millington@uq.edu.au>
2026-09-08 16:47:44 +1000

    chore: initialise clean slate for form package

---
+++ packages/storybook-html/stories/components/form/form.stories.js
@@ -0,0 +1,24 @@
+export default {
+  title: "Components/Form",
+  parameters: {
+    previewTabs: {
+      canvas: {
+        hidden: false,
+      },
+    },
+  },
+  render: () => {
+    return `
+    <div>
+      <div class="uq-form">
+        <form>
+          Form content goes here
+        </form>
+      </div>
+    </div>
+    `;
+  },
+};
+
+
+export const Default = {};

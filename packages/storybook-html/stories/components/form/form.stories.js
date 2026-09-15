d11f18d115befa6f55bcf4720aee7e51c51b3009
Nate Millington <n.millington@uq.edu.au>
2026-09-15 14:46:32 +1000

    fix: add disabled states

--- packages/storybook-html/stories/components/form/form.stories.js
+++ packages/storybook-html/stories/components/form/form.stories.js
@@ -28,7 +28,7 @@ export default {
  
             <div class="uq-form__group uq-form__group--inline">
               <label class="uq-form__label" for="email-address">Email</label>
-              <input class="uq-form__input" id="email-address" type="email" name="email-address">
+              <input disabled class="uq-form__input" id="email-address" type="email" name="email-address">
             </div>
           </fieldset>
           <fieldset class="uq-form__fieldset">

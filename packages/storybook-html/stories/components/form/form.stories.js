66b0ffd80c414314c7bdfd53dd457c0fc5e5c3e2
Nate Millington <n.millington@uq.edu.au>
2026-09-15 16:55:13 +1000

    fix: added implicit and explicit examples for comparison. fixed inline input for implicit markup.

--- packages/storybook-html/stories/components/form/form.stories.js
+++ packages/storybook-html/stories/components/form/form.stories.js
@@ -10,31 +10,58 @@ export default {
   render: () => {
     return `
       <div>
+      <div class="uq-grid uq-grid--2-column">
         <form class="uq-form">
           <fieldset class="uq-form__fieldset">
             <legend class="uq-form__legend">
-              <h5>Text Inputs</h5>
+              <h3>Text Inputs</h3>
+              <span class="text--xs uq-icon uq-icon--standard--exclamation-circle">Explicit</span>
             </legend>
+            <h4>Default</h4>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="text-input">Text input</label>
+              <input class="uq-form__input" id="text-input" type="text" name="text-input">
+            </div>
+        
+            
+            <h4>With helper text</h4>
             <div class="uq-form__group">
               <label class="uq-form__label" for="text-input">Text input</label>
               <span class="uq-form__helper">Help text or description under label</span>
               <input class="uq-form__input" id="text-input" type="text" name="text-input">
             </div>
  
+            <h4>In-line text input</h4>
             <div class="uq-form__group uq-form__group--inline">
               <label class="uq-form__label" for="email-address">In-line text input</label>
               <input class="uq-form__input" id="email-address" type="email" name="email-address">
             </div>
- 
+
+            <h4>Disabled states</h4>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="text-input">Text input</label>
+              <span class="uq-form__helper">Help text or description under label</span>
+              <input disabled class="uq-form__input" id="text-input" type="text" name="text-input">
+            </div>
+
             <div class="uq-form__group uq-form__group--inline">
               <label class="uq-form__label" for="email-address">Disabled</label>
               <input disabled class="uq-form__input" id="email-address" type="email" name="email-address">
             </div>
+
+            <h4>Error states</h4>
+            <div class="uq-form__group uq-form__group--error">
+              <label class="uq-form__label" for="text-input">Text input</label>
+              <span class="uq-form__helper">Error text goes here</span>
+              <input class="uq-form__input" id="text-input" type="text" name="text-input">
+            </div>
+
           </fieldset>
           <fieldset class="uq-form__fieldset">
             <legend class="uq-form__legend">
               <h3>Select Inputs</h3>
             </legend>
+
             <div class="uq-form__group">
               <label class="uq-form__label" for="course">Select</label>
               <span class="uq-form__helper">Help text or description under label</span>
@@ -45,7 +72,7 @@ export default {
                 <option value="bachelor-of-science">Bachelor of Science</option>
               </select>
             </div>
- 
+
             <div class="uq-form__group uq-form__group--inline">
               <label class="uq-form__label" for="course">In-line select</label>
               <select class="uq-form__select" id="course" name="course">
@@ -54,6 +81,159 @@ export default {
                 <option value="bachelor-of-engineering">Bachelor of Engineering</option>
                 <option value="bachelor-of-science">Bachelor of Science</option>
               </select>
+            </div>
+
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="course">Select disabled</label>
+              <span class="uq-form__helper">Help text or description under label</span>
+              <select disabled class="uq-form__select" id="course" name="course">
+                <option value="">Select a course</option>
+                <option value="bachelor-of-commerce">Bachelor of Commerce</option>
+                <option value="bachelor-of-engineering">Bachelor of Engineering</option>
+                <option value="bachelor-of-science">Bachelor of Science</option>
+              </select>
+            </div>
+
+          </fieldset>
+ 
+          <fieldset class="uq-form__fieldset">
+            <legend class="uq-form__legend">
+              <h3>Multiline Text Input</h3>
+            </legend>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="message">Multiline text input</label>
+              <span class="uq-form__helper">Helper text for the message</span>
+              <textarea class="uq-form__input" id="message" name="message" rows="4"></textarea>
+            </div>
+
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="message">Multiline text input</label>
+              <span class="uq-form__helper">Helper text for the message</span>
+              <textarea disabled class="uq-form__input" id="message" name="message" rows="4"></textarea>
+            </div>
+          </fieldset>
+
+          <fieldset class="uq-form__fieldset">
+            <legend class="uq-form__legend">
+              <h3>Input groups</h3>
+            </legend>
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button">Search</button>
+              </div>
+            </div>
+
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button"><span class="uq-icon uq-icon--standard--search"></span> <span class="sr-only">Search</span></button>
+              </div>
+            </div>
+
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button uq-button--tertiary"><span class="uq-icon uq-icon--standard--search"></span> <span class="sr-only">Search</span></button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+
+        <form class="uq-form">
+          <fieldset class="uq-form__fieldset">
+            <legend class="uq-form__legend">
+              <h3>Text Inputs</h3>
+              <span class="text--xs uq-icon uq-icon--standard--information-circle">Implicit</span>
+            </legend>
+
+            <h4>Default</h4>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="text-input">Text input
+                <input class="uq-form__input" id="text-input" type="text" name="text-input">
+              </label>
+            </div>
+            
+            <h4>With helper text</h4>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="text-input">Text input
+                <span class="uq-form__helper">Help text or description under label</span>
+                <input class="uq-form__input" id="text-input" type="text" name="text-input">
+              </label>
+            </div>
+ 
+            <h4>In-line text input</h4>
+            <div class="uq-form__group uq-form__group--inline">
+              <label class="uq-form__label" for="email-address">In-line text input
+                <input class="uq-form__input" id="email-address" type="email" name="email-address">
+              </label>
+            </div>
+
+            <h4>Disabled states</h4>
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="text-input">Text input
+                <span class="uq-form__helper">Help text or description under label</span>
+                <input disabled class="uq-form__input" id="text-input" type="text" name="text-input">
+              </label>
+            </div>
+ 
+            <div class="uq-form__group uq-form__group--inline">
+              <label class="uq-form__label" for="email-address">Disabled
+                <input disabled class="uq-form__input" id="email-address" type="email" name="email-address">
+              </label>
+            </div>
+
+            <h4>Error states</h4>
+            <div class="uq-form__group uq-form__group--error">
+              <label class="uq-form__label" for="text-input">Text input
+                <span class="uq-form__helper">Error text goes here</span>
+                <input class="uq-form__input" id="text-input" type="text" name="text-input">
+              </label>
+            </div>
+          </fieldset>
+          <fieldset class="uq-form__fieldset">
+            <legend class="uq-form__legend">
+              <h3>Select Inputs</h3>
+            </legend>
+
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="course">Select
+                <span class="uq-form__helper">Help text or description under label</span>
+                <select class="uq-form__select" id="course" name="course">
+                  <option value="">Select a course</option>
+                  <option value="bachelor-of-commerce">Bachelor of Commerce</option>
+                  <option value="bachelor-of-engineering">Bachelor of Engineering</option>
+                  <option value="bachelor-of-science">Bachelor of Science</option>
+                </select>
+              </label>
+            </div>
+
+            <div class="uq-form__group uq-form__group--inline">
+              <label class="uq-form__label" for="course">In-line select
+                <select class="uq-form__select" id="course" name="course">
+                  <option value="">Select a course</option>
+                  <option value="bachelor-of-commerce">Bachelor of Commerce</option>
+                  <option value="bachelor-of-engineering">Bachelor of Engineering</option>
+                  <option value="bachelor-of-science">Bachelor of Science</option>
+              </select>
+              </label>
+            </div>
+
+
+
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="course">Select disabled
+                <span class="uq-form__helper">Help text or description under label</span>
+                <select disabled class="uq-form__select" id="course" name="course">
+                  <option value="">Select a course</option>
+                  <option value="bachelor-of-commerce">Bachelor of Commerce</option>
+                  <option value="bachelor-of-engineering">Bachelor of Engineering</option>
+                  <option value="bachelor-of-science">Bachelor of Science</option>
+                </select>
+              </label>
             </div>
           </fieldset>
  
@@ -64,15 +244,49 @@ export default {
             <div class="uq-form__group">
               <label class="uq-form__label" for="message">Multiline text input</label>
               <span class="uq-form__helper">Helper text for the message</span>
-              <textarea class="uq-form__input" id="message" name="message" rows="4" placeholder="Tell us about your enquiry"></textarea>
+              <textarea class="uq-form__input" id="message" name="message" rows="4"></textarea>
             </div>
+
+            <div class="uq-form__group">
+              <label class="uq-form__label" for="message">Multiline text input</label>
+              <span class="uq-form__helper">Helper text for the message</span>
+              <textarea disabled class="uq-form__input" id="message" name="message" rows="4"></textarea>
+            </div>
+          </fieldset>
+
+          <fieldset class="uq-form__fieldset">
+            <legend class="uq-form__legend">
+              <h3>Input groups</h3>
+            </legend>
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button">Search</button>
+              </div>
+            </div>
+
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button"><span class="uq-icon uq-icon--standard--search"></span> <span class="sr-only">Search</span></button>
+              </div>
+            </div>
+
+            <div class="uq-form__group">
+              <div class="uq-form__input-group">
+                <label class="sr-only" for="search">Search</label>
+                <input id="search" type="text" name="search">
+                <button type="submit" class="uq-button uq-button--tertiary"><span class="uq-icon uq-icon--standard--search"></span> <span class="sr-only">Search</span></button>
+              </div>
+            </div>
+
           </fieldset>
 
         </form>
       </div>
- 
-        </form>
-      </div>
+
 
 <hr> 
 <h2>Kitchen sink form</h2>
